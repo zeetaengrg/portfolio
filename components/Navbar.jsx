ac83f8e591d3a1998c008700ea995455b04e24be
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-29 22:01:02 +0545

    style: linkStyle object created for styling
    - linkStyle object contains all the common styles of Link component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,13 @@ import { Box, Flex, Image, Link, Spacer } from "@chakra-ui/react";
 import NextLink from "next/link";
 
 const Navbar = () => {
+
+    const linkStyle = {
+        _hover: {
+            textDecoration: "none",
+        }
+    };
+
     return (
         <Flex p={{ base: "1rem 0.5rem", md: "1rem 4rem", lg: "1rem 8rem" }}>
             <Box>
@@ -14,16 +21,16 @@ const Navbar = () => {
             <Spacer />
             <Flex gap={{ base: "1.2rem", md: "2rem", lg: "4rem" }} alignItems="center">
                 <NextLink href="/">
-                    <Link _hover={{ textDecoration: "none" }}>About Me</Link>
+                    <Link sx={linkStyle}>About Me</Link>
                 </NextLink>
                 <NextLink href="#skills">
-                    <Link _hover={{ textDecoration: "none" }}>Skills</Link>
+                    <Link sx={linkStyle}>Skills</Link>
                 </NextLink>
                 <NextLink href="#projects">
-                    <Link _hover={{ textDecoration: "none" }}>Projects</Link>
+                    <Link sx={linkStyle}>Projects</Link>
                 </NextLink>
                 <NextLink href="#contact">
-                    <Link _hover={{ textDecoration: "none" }}>Contact</Link>
+                    <Link sx={linkStyle}>Contact</Link>
                 </NextLink>
             </Flex>
         </Flex>
