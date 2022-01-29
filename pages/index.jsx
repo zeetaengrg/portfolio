4650e322452a7616fe7e5301aeca1ce0dcca463a
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-29 15:26:01 +0545

    feat: Skills component added to the layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,13 @@
 import Intro from "../components/Intro";
 import Navbar from "../components/Navbar";
+import Skills from "../components/Skills";
 
 const Home = () => {
 	return (
 		<div>
 			<Navbar />
 			<Intro />
+			<Skills />
 		</div>
 	);
 };
