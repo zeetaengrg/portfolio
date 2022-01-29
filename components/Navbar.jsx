b22c8154438f827b56b39cda683c37e4facaf5e0
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-29 15:16:02 +0545

    feat and style: NextLink component added and responsiveness added
    - NextLink component of Nextjs is used to wrap Link components of chakra UI.
    - nav links responsiveness added accordingly

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,41 +1,33 @@
-import { Box, Flex, Image, Link, Spacer } from '@chakra-ui/react';
-import NextLink from 'next/link';
+import { Box, Flex, Image, Link, Spacer } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 const Navbar = () => {
-  	return (
-		<Flex p='1rem 8rem'>
-			<Box>
-				<NextLink href='/'>
-					<Link>
-							<Image src='/images/logo.png' alt='Logo' />
-					</Link>
-				</NextLink>
-			</Box>
-			<Spacer />
-			<Flex gap='4rem' alignItems='center'>
-				<NextLink href='/'>
-					<Link _hover={{ textDecoration: 'none' }}>
-						About Me
-					</Link>
-				</NextLink>
-				<NextLink href='#skills'>
-					<Link _hover={{ textDecoration: 'none' }}>
-						Skills
-					</Link>
-				</NextLink>
-				<NextLink href='#projects'>
-					<Link _hover={{ textDecoration: 'none' }}>
-						Projects
-					</Link>
-				</NextLink>
-				<NextLink href='#contact'>
-					<Link _hover={{ textDecoration: 'none' }}>
-						Contact
-					</Link>
-				</NextLink>
-			</Flex>
-		</Flex>
-  	)
+    return (
+        <Flex p={{ base: "1rem 0.5rem", md: "1rem 4rem", lg: "1rem 8rem" }}>
+            <Box>
+                <NextLink href="/">
+                    <Link>
+                        <Image src="/images/logo.png" alt="Logo" />
+                    </Link>
+                </NextLink>
+            </Box>
+            <Spacer />
+            <Flex gap={{ base: "1.2rem", md: "2rem", lg: "4rem" }} alignItems="center">
+                <NextLink href="/">
+                    <Link _hover={{ textDecoration: "none" }}>About Me</Link>
+                </NextLink>
+                <NextLink href="#skills">
+                    <Link _hover={{ textDecoration: "none" }}>Skills</Link>
+                </NextLink>
+                <NextLink href="#projects">
+                    <Link _hover={{ textDecoration: "none" }}>Projects</Link>
+                </NextLink>
+                <NextLink href="#contact">
+                    <Link _hover={{ textDecoration: "none" }}>Contact</Link>
+                </NextLink>
+            </Flex>
+        </Flex>
+    );
 };
 
 export default Navbar;
