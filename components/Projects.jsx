ba3af1d5d87793c469b844f2c39ba264ced77232
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-29 22:02:10 +0545

    style: Projects component styled
    - All the lists of projects to be displayed are styled accordingly using map array

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,11 +1,99 @@
-import { Heading } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { Center, Divider, Flex, Grid, Heading, Image, Text, Link } from "@chakra-ui/react";
+import { projects } from "../data/data";
 
 const Projects = () => {
+
+    const n = projects.length;
+
+    const linkStyle = {
+        _hover: {
+            textDecoration: "none",
+            "&::after": { content: "none" }
+        }
+    };
+
+    const imgStyle = {
+        width: "250rem",
+        transition: "all 0.3s ease-in-out",
+        _hover: {
+            transform: "scale(1.05)",
+            transition: "all 0.3s ease-in-out"
+        },
+        borderRadius: "0.5rem",
+        boxShadow: "0 0.5rem 1.5rem 1rem rgba(0,0,0,0.5)",
+        objectFit: "contain"
+    };
+
     return (
         <>
-            <Heading>
+            <Heading
+                mt={{ base: "2rem", md: "4rem" }}
+                id="projects"
+                textAlign="center"
+                textTransform="uppercase"
+                bgGradient="linear(to-r, #fff, #B4A5A5, #3C415C, #0F3460, #16213E)"
+                bgClip="text"
+                fontSize={{ base: "3rem", md: "4rem" }}
+                fontFamily="inherit"
+            >
                 Projects
             </Heading>
+            <Center>
+                <Divider width="15%" />
+            </Center>
+            {projects.map((project) => (
+                <>
+                    <Flex p="2rem 6rem" gap="2rem" justifyContent="center">
+                        <NextLink href="#">
+                            <Link sx={linkStyle}>
+                                <Image
+                                    src={project.imageOne}
+                                    alt="Hulu Movie Streaming App"
+                                    sx={imgStyle}
+                                />
+                            </Link>
+                        </NextLink>
+                        <Grid placeContent="center">
+                            <Text>
+                                <Heading as="h3" fontSize="2rem">
+                                    {project.titleOne}
+                                </Heading>
+                                {project.descriptionOne}
+                            </Text>
+                            <Flex mt="0.5rem" gap="2rem">
+                                {project.technologyOne.map((tech) => (
+                                    <Image src={tech.image} alt={tech.name} key={tech.id} height="48px" />
+                                ))}
+                            </Flex>
+                        </Grid>
+                    </Flex>
+                    <Flex p="2rem 6rem" gap="2rem" justifyContent="center">
+                        <Grid placeContent="center">
+                            <Text>
+                                <Heading as="h3" fontSize="2rem">
+                                    {project.titleTwo}
+                                </Heading>
+                                {project.descriptionTwo}
+                            </Text>
+                            <Flex mt="0.5rem" gap="2rem">
+                                {project.technologyTwo.map((tech) => (
+                                    <Image src={tech.image} alt={tech.name} key={tech.id} height="48px" />
+                                ))}
+                            </Flex>
+                        </Grid>
+                        <NextLink href="#">
+                            <Link sx={linkStyle}>
+                                <Image
+                                    src={project.imageTwo}
+                                    alt="Hulu Movie Streaming App"
+                                    sx={imgStyle}
+                                />
+                            </Link>
+                        </NextLink>
+                    </Flex>
+                </>
+            ))}
         </>
     );
 };
