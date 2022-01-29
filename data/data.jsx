e0a23d83a71ddbe0174f10427469ad1efba308fe
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-29 22:08:19 +0545

    data.jsx created which contains local data contents

---
+++ data/data.jsx
@@ -0,0 +1,87 @@
+export const projects = [
+    {
+        id: 1,
+        titleOne: "Hulu Landing Page",
+        titleTwo: "Portfolio Website",
+        descriptionOne:
+            "This is a simple clone of Hulu's landing page. Built from scratch with pixel perfect design similar to Hulu's Website. This is a fully responsive page. A little bit of Javascript has been implemented for 'Login' modal functionality.",
+        descriptionTwo:
+            "This is a portfolio website which provides a simple way to showcase the tools and technologies I use, projects I have worked or been working as well as simple way to contact me. This is a fully responsive page. Chakra UI framework has been used for the front-end design.",
+        imageOne: "/images/hulu.png",
+        imageTwo: "/images/portfolio.png",
+        technologyOne: [
+            {
+                id: 1,
+                name: "HTML",
+                image: "/images/html.png",
+            },
+            {
+                id: 2,
+                name: "CSS",
+                image: "/images/css.png",
+            },
+            {
+                id: 3,
+                name: "JavaScript",
+                image: "/images/javascript.png",
+            }
+        ],
+        technologyTwo: [
+            {
+                id: 1,
+                name: "NextJS",
+                image: "/images/nextjs.png",
+            },
+            {
+                id: 2,
+                name: "Chakra-UI",
+                image: "/images/chakraui.png",
+            }
+        ]
+    },
+    {
+        id: 2,
+        titleOne: "E-Commerce Website",
+        titleTwo: "Form Validation",
+        descriptionOne:
+            "This is a clothing store e-commerce website which provides functionalities to sell clothing products.",
+        descriptionTwo:
+            "This is a form validation which validates the form fields and shows the error messages if the form is not valid.",
+        imageOne: "/images/clothingstore.png",
+        imageTwo: "/images/form-validation.png",
+        technologyOne: [
+            {
+                id: 1,
+                name: "ReactJS",
+                image: "/images/reactjs.png",
+            },
+            {
+                id: 2,
+                name: "Styled-Components",
+                image: "/images/styled-components.png",
+            },
+            {
+                id: 3,
+                name: "Materail-UI",
+                image: "/images/materialui.png",
+            }
+        ],
+        technologyTwo: [
+            {
+                id: 1,
+                name: "HTML",
+                image: "/images/html.png",
+            },
+            {
+                id: 2,
+                name: "CSS",
+                image: "/images/css.png",
+            },
+            {
+                id: 3,
+                name: "JavaScript",
+                image: "/images/javascript.png",
+            }
+        ]
+    },
+];
