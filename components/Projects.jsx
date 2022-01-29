76a86764f945aa9b6ceb9df74684d8c7304b72bb
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-29 18:40:01 +0545

    feat: Projects component created

---
+++ components/Projects.jsx
@@ -0,0 +1,13 @@
+import { Heading } from "@chakra-ui/react";
+
+const Projects = () => {
+    return (
+        <>
+            <Heading>
+                Projects
+            </Heading>
+        </>
+    );
+};
+
+export default Projects;
