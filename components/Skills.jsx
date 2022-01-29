7b5e3007dddeeeb96efe470706398967658c86a3
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-29 15:23:57 +0545

    feat: Skills component added
    - this component contains list of tools and technologies I use on a daily basis

---
+++ components/Skills.jsx
@@ -0,0 +1,11 @@
+import { Heading } from "@chakra-ui/react";
+
+const Skills = () => {
+    return (
+        <Heading id="skills" textAlign="center" textTransform="uppercase">
+            Tools and Technologies
+        </Heading>
+    );
+};
+
+export default Skills;
