df1c0b84a45145c6dfe112c4b5e8a924d143f327
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-30 10:33:18 +0545

    feat and style: Stack component used and contact email Link updated
    - Stack component used for better spacing simplicity between input forms.
    - email contact set to directly open the mailbox for simplicity

--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -43,42 +43,68 @@ const Contacts = () => {
             <Center>
                 <Divider width="25%" />
             </Center>
-            <Flex p="2rem 6rem" gap="2rem" justifyContent="center">
+            <Flex p="1rem 6rem" gap="2rem" justifyContent="center">
                 <FormControl mt="2rem">
-                    <FormLabel htmlFor="name">Name</FormLabel>
-                    <Input placeholder="Name" type="name" id="name" isRequired />
-                    <FormLabel htmlFor="email">Email</FormLabel>
-                    <Input
-                        placeholder="Email"
-                        id="email"
-                        type="email"
-                        value={input}
-                        onChange={handleInputChange}
-                        isRequired
-                    />
-                    {isError && <FormErrorMessage>Please enter your email</FormErrorMessage>}
-                    <FormLabel htmlFor="message">Message</FormLabel>
-                    <Textarea placeholder="Message" id="message" />
-                    <Button variantColor="teal" mt="2rem">Send</Button>
+                    <Stack spacing="4">
+                        <Input
+                            placeholder="Name"
+                            type="name"
+                            id="name"
+                            width="25rem"
+                            isRequired
+                        />
+                        <Input
+                            placeholder="Email"
+                            id="email"
+                            type="email"
+                            value={input}
+                            onChange={handleInputChange}
+                            width="25rem"
+                            isRequired
+                        />
+                        {isError && (
+                            <FormErrorMessage>
+                                Please enter your email
+                            </FormErrorMessage>
+                        )}
+                        <Textarea
+                            placeholder="Message"
+                            id="message"
+                            width="25rem"
+                            height="10rem"
+                            isRequired
+                        />
+                    </Stack>
+                    <Button
+                        mt="1rem"
+                        width="25rem"
+                        variant="outline"
+                        sx={btnStyle}
+                    >
+                        Send
+                    </Button>
                 </FormControl>
                 <Flex flexDirection="column">
                     <Flex>
                         <ImLocation />
-                        <Text>
-                            Pokhara, Nepal
-                        </Text>
+                        <Text>Pokhara, Nepal</Text>
                     </Flex>
                     <Flex>
                         <FaMobileAlt />
-                        <Text>
-                            +977 9814010101
-                        </Text>
+                        <Text>+977 9814010101</Text>
                     </Flex>
                     <Flex>
                         <HiMail />
-                        <Text>
-                            zeetaen.gurung@gmail.com
-                        </Text>
+                        <Link
+                            href="mailto:zeetaen.gurung@gmail.com"
+                            _hover={{
+                                "&::after": { content: "none" },
+                                color: "#B4A5A5",
+                            }}
+                            textTransform="none"
+                        >
+                            <Text>zeetaen.gurung@gmail.com</Text>
+                        </Link>
                     </Flex>
                 </Flex>
             </Flex>
