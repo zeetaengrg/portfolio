3c58f1e52716d5cf08c50412e68bb0e3daccda3e
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-30 11:29:34 +0545

    feat: Footer component created and edited

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import { Center, Divider, Text, Flex } from '@chakra-ui/react';
+import { FaCopyright } from 'react-icons/fa';
 
 const Footer = () => {
     return (
         <>
-            
+            <Center>
+                <Divider width="95%" />
+            </Center>
+            <Text fontSize="0.8rem" m="0.5rem">
+                <Flex justifyContent="center" alignItems="center">
+                    2022 <FaCopyright style={{margin: "0 0.5rem"}} /> Jiten Gurung. All Rights Reserved.
+                </Flex>
+            </Text>
         </>
     );
 };
