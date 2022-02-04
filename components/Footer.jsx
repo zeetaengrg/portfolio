1d8ec196cd834e253cac75bd9bb47662a68f5364
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-04 23:06:16 +0545

    feat: Footer section reconfigured and restyled to new look

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,42 @@
-import { Center, Divider, Text, Flex } from '@chakra-ui/react';
-import { FaCopyright } from 'react-icons/fa';
+import { Center, Text, Flex, Image, Grid, Link } from '@chakra-ui/react';
+import { FaCopyright, FaFacebookSquare, FaInstagramSquare, FaYoutubeSquare } from 'react-icons/fa';
 
 const Footer = () => {
+
+    const iconStyle = {
+        fontSize: "1.5rem",
+    }
+
+    const linkStyle = {
+        _hover: {
+            "&::after": { content: "none" },
+            color: "#a5abbd",
+        }
+    }
+
     return (
         <>
-            <Center>
-                <Divider width="95%" />
-            </Center>
-            <Text fontSize="0.8rem" m="0.5rem">
-                <Flex justifyContent="center" alignItems="center">
-                    2022 <FaCopyright style={{margin: "0 0.5rem"}} /> Jiten Gurung. All Rights Reserved.
+            <Grid bgColor="#141E39" placeContent="center">
+                <Center>
+                    <Image src="/images/logo.png" alt="logo" m="2rem 0" />
+                </Center>
+                <Flex mb="1rem" gap="2rem" justifyContent="center">
+                    <Link href="https://www.facebook.com/zeetaen/" sx={linkStyle} target="_blank" rel="noreferrer noopener nofollow">
+                        <FaFacebookSquare style={iconStyle} />
+                    </Link>
+                    <Link href="https://www.instagram.com/zeetaen/" sx={linkStyle} target="_blank" rel="noreferrer noopener nofollow">
+                        <FaInstagramSquare style={iconStyle} />
+                    </Link>
+                    <Link href="https://www.youtube.com/c/ZeetaenGurung" sx={linkStyle} target="_blank" rel="noreferrer noopener nofollow">
+                        <FaYoutubeSquare style={iconStyle} />
+                    </Link>
                 </Flex>
-            </Text>
+                <Text fontSize="0.8rem" m="0.5rem">
+                    <Flex justifyContent="center" alignItems="center">
+                        2022 <FaCopyright style={{margin: "0 0.5rem"}} /> Jiten Gurung. All Rights Reserved.
+                    </Flex>
+                   </Text>
+            </Grid>
         </>
     );
 };
