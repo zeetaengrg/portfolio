8c7b2791ef41dd68067b814e63c5f1dfb0af02e8
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-04 16:27:04 +0545

    style: backgroundcolor and font color restyled
    - previous bgcolor and fontcolor restyled

--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -39,8 +39,8 @@ const Contacts = () => {
         fontSize: "1.1rem",
         textTransform: "uppercase",
         _focus: { boxShadow: "none" },
-        _hover: { bg: "#B4A5A5", color: "#0F3460", borderColor: "#0F3460" },
-        _active: { bg: "#B4A5A5", color: "#0F3460", transform: "scale(0.98)" },
+        _hover: { bg: "#a5abbd", color: "#384765", borderColor: "#0F3460" },
+        _active: { bg: "#a5abbd", color: "#384765", transform: "scale(0.98)" },
     }
 
     const iconStyle = {
@@ -59,7 +59,7 @@ const Contacts = () => {
     const linkStyle = {
         _hover:{
             "&::after": { content: "none" },
-            color: "#B4A5A5",
+            color: "#a5abbd",
         },
         textTransform:"none",
         _focus: { boxShadow: "none" },
@@ -81,7 +81,7 @@ const Contacts = () => {
                 id="contact"
                 textAlign="center"
                 textTransform="uppercase"
-                bgGradient="linear(to-r, #fff, #B4A5A5, #3C415C, #0F3460, #16213E)"
+                bgGradient="linear(to-r, #fff, #a5abbd, #384765, #152641, #0B111E)"
                 bgClip="text"
                 fontSize={{ base: "3rem", md: "4rem" }}
                 fontFamily="inherit"
@@ -125,10 +125,10 @@ const Contacts = () => {
                             type="name"
                             id="name"
                             width={{ base: "23rem", md: "25rem" }}
-                            bgColor="#16213E"
-                            color="#B4A5A5"
-                            value={values[input.name]}
-                            onChange={handleChange}
+                            bgColor="#384765"
+                            color="#a5abbd"
+                            // value={values[input.name]}
+                            // onChange={handleChange}
                             isRequired
                         />
                         <Input
@@ -136,11 +136,11 @@ const Contacts = () => {
                             name="email"
                             id="email"
                             type="email"
-                            value={values[input.name]}
-                            onChange={handleChange}
+                            // value={values[input.name]}
+                            // onChange={handleChange}
                             width={{ base: "23rem", md: "25rem" }}
-                            bgColor="#16213E"
-                            color="#B4A5A5"
+                            bgColor="#384765"
+                            color="#a5abbd"
                             isRequired
                         />
                         {isError && (
