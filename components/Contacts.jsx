58b35bb18831071bbf222f37e1492dfc0eeef486
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-04 21:09:31 +0545

    refactor: socia icon center restyling

--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -41,38 +41,42 @@ const Contacts = () => {
         _focus: { boxShadow: "none" },
         _hover: { bg: "#a5abbd", color: "#384765", borderColor: "#0F3460" },
         _active: { bg: "#a5abbd", color: "#384765", transform: "scale(0.98)" },
-    }
+    };
 
     const iconStyle = {
         fontSize: "2rem",
-    }
+    };
 
     const textStyle = {
         fontSize: "1.2rem",
-    }
+    };
 
     const flexStyle = {
         gap: "1rem",
         alignItems: "center",
-    }
+    };
 
     const linkStyle = {
-        _hover:{
+        _hover: {
             "&::after": { content: "none" },
             color: "#a5abbd",
         },
-        textTransform:"none",
+        textTransform: "none",
         _focus: { boxShadow: "none" },
-    }
+    };
 
     const socialLinkStyle = {
-        textTransform:"none",
+        textTransform: "none",
         _focus: { boxShadow: "none" },
         fontSize: "2rem",
         cursor: "pointer",
         transition: "all 0.2s ease-out",
-        _hover: { transform: "translateY(5px)", color: "#fff", "&::after": { content: "none" }},
-    }
+        _hover: {
+            transform: "translateY(5px)",
+            color: "#fff",
+            "&::after": { content: "none" },
+        },
+    };
 
     return (
         <>
@@ -202,7 +206,7 @@ const Contacts = () => {
                         </Link>
                     </Flex>
                     <Divider />
-                    <Flex gap="5rem" justifyContent="center">
+                    <Flex gap="7rem">
                         <Link
                             sx={socialLinkStyle}
                             href="https://www.linkedin.com/in/jitengrg"
