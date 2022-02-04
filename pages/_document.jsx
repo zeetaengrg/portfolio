77bc5433852ea7095e19d78b2c375e53f4843813
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-04 21:15:03 +0545

    refactor

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,6 +13,7 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Poppins:wght@300;400;500;600;700&family=Roboto+Mono:ital,wght@1,100&family=Roboto:wght@100;300;400;500;700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;0,900;1,300&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
+          <form action="https://formsubmit.co/zeetaen.gurung@gmail.com" method="POST"></form>
           <Main />
           <NextScript />
         </body>
