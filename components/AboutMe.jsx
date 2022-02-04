63779d4448ab539da3fd84575c29950fdcb96e96
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-04 23:04:59 +0545

    refactor: styling reconfigured and about me section added instead of aboutme page

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -3,28 +3,20 @@ import {
     Heading,
     Center,
     Image,
-    useMediaQuery,
     Text,
     Link,
-    Grid,
-    Button,
-    Flex,
+    Flex
 } from "@chakra-ui/react";
-import { FaCloudDownloadAlt, FaHandshake } from "react-icons/fa";
-import { RiArrowGoBackFill } from "react-icons/ri";
-import NextLink from "next/link"
 
 const AboutMe = () => {
-
-    const [isNotSmallerScreen] = useMediaQuery("(min-width: 600px)");
 
     const btnStyle = {
         padding: "0 0.8rem",
         backgroundColor: "#16213E",
         fontSize: "1.2rem",
         _focus: { boxShadow: "none" },
-        _hover: { bg: "#B4A5A5", color: "#0F3460", borderColor: "#0F3460" },
-        _active: { bg: "#B4A5A5", color: "#0F3460", transform: "scale(0.98)" },
+        _hover: { bg: "#a5abbd", color: "#384765", borderColor: "#0F3460" },
+        _active: { bg: "#a5abbd", color: "#384765", transform: "scale(0.98)" },
     };
 
     const linkStyle = {
@@ -34,41 +26,33 @@ const AboutMe = () => {
     }
 
     return (
-        <Flex flexDirection="column" height={{ base: "84.8vh", md: "81.5vh" }}>
+        <Flex direction="column">
             <Heading
                 mt={{ base: "1rem", md: "2rem" }}
-                id="skills"
+                id="about"
                 textAlign="center"
                 textTransform="uppercase"
-                bgGradient="linear(to-r, #fff, #B4A5A5, #3C415C, #0F3460, #16213E)"
+                bgGradient="linear(to-r, #fff, #a5abbd, #384765, #152641, #0B111E)"
                 bgClip="text"
                 fontSize={{ base: "3rem", md: "4rem" }}
                 fontFamily="inherit"
             >
-                About Me
+                Who Am I ?
             </Heading>
             <Center>
-                <Divider width={{ base: "40%", md: "20%" }} />
+                <Divider width={{ base: "40%", md: "18%" }}  mb="0.5rem" />
             </Center>
             <Center>
-                <Image
-                    src="/images/developer.svg"
-                    alt="Developer SVG"
-                    {...(isNotSmallerScreen
-                        ? { display: "block" }
-                        : { display: "none" })}
-                    opacity="0.2"
-                    position="absolute"
-                    bottom="3rem"
-                    width="50rem"
-                />
+                <Text>
+                    My Introduction
+                </Text>
             </Center>
             <Text
                 textAlign="center"
                 fontSize={{ base: "1.2rem", md: "1.5rem" }}
-                p={{ base: "1rem 1.5rem", md: "3rem 6rem" }}
+                p={{ base: "1rem 1.5rem", md: "2rem 6rem" }}
             >
-                An aspiring Front-End Web Developer from{" "}
+                An Front-End Web Developing aficionado from{" "}
                 <Link
                     href="https://en.wikipedia.org/wiki/Nepal"
                     target="_blank"
@@ -79,52 +63,10 @@ const AboutMe = () => {
                     🇳🇵
                 </Link>{" "}
                 with an acute detail to hone the skills as required by the
-                project requirement. Web technology aficionado with a bit of UI
+                project requirement. A Web technology ethusiast with a bit of UI
                 designing proficiency and immense interest to learn new tech stacks.
             </Text>
-            <Flex
-                justifyContent="center"
-                alignItems="center"
-                gap="1rem"
-                mt="4rem"
-                mb="2rem"
-            >
-                <Link
-                    href="/cv/Resume.pdf"
-                    download
-                    _hover={{ "&::after": { content: "none" } }}
-                >
-                    <Button
-                        variant="outline"
-                        size="lg"
-                        sx={btnStyle}
-                        rightIcon={
-                            <FaCloudDownloadAlt
-                                style={{ fontSize: "1.5rem" }}
-                            />
-                        }
-                    >
-                        Download CV
-                    </Button>
-                </Link>
-                <NextLink href="/#contact">
-                    <Link
-                        _hover={{ "&::after": { content: "none" } }}
-                    >
-                        <Button
-                            variant="outline"
-                            size="lg"
-                            sx={btnStyle}
-                            rightIcon={
-                                <FaHandshake style={{ fontSize: "1.5rem" }} />
-                            }
-                        >
-                            Hire Me
-                        </Button>
-                    </Link>
-                </NextLink>
-            </Flex>
-            <Grid placeContent="center" mb="1rem">
+            {/* <Grid placeContent="center" mb="1rem">
                 <NextLink href="/">
                     <Link href="/" _hover={{ "&::after": { content: "none" } }}>
                         <Button sx={btnStyle} variant="outline" size="lg" rightIcon={<RiArrowGoBackFill />}>
@@ -132,7 +74,7 @@ const AboutMe = () => {
                         </Button>
                     </Link>
                 </NextLink>
-            </Grid>
+            </Grid> */}
         </Flex>
     );
 };
