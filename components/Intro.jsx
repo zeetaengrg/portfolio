ca2e9e049e0cf350d9ef691bab39b3d6ef3eeeed
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-05 17:30:14 +0545

    refactor(component): Box component added to encapsulate Flex and Text component

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -13,98 +13,106 @@ const Intro = () => {
     };
 
     return (
-        <Flex
-            justifyContent="center"
-            alignItems="center"
-            height={{ base: "55vh", md: "60vh", lg: "87vh" }}
-            gap={{ base: "1rem", md: "2rem", lg: "2rem" }}
-            direction={{ base: "column", md: "row" }}
-        >
-            <Image
-                src="/images/jiten.jpg"
-                alt="Jiten Image"
-                borderRadius="50%"
-                boxShadow="0px 0px 10px 10px rgba(0,0,0,0.5)"
-                width={{ base: "8rem", md: "12rem", lg: "17rem" }}
-            />
+        <>
             <Flex
-                direction="column"
-                textAlign={{ base: "center", md: "start" }}
+                justifyContent="center"
+                alignItems="center"
+                height={{ base: "55vh", md: "60vh", lg: "87vh" }}
+                gap={{ base: "1rem", md: "2rem", lg: "2rem" }}
+                direction={{ base: "column", md: "row" }}
             >
-                <Text
-                    casing="uppercase"
-                    fontSize={{ base: "1rem", md: "1.5rem" }}
-                >
-                    Hi!
-                </Text>
-                <Text
-                    casing="uppercase"
-                    fontSize={{ base: "1.5rem", md: "2rem" }}
-                    fontWeight="bold"
-                >
-                    <Flex justifyContent={{ base: "center", md: "start" }}>
-                        I&apos;m
-                        <FaQuoteLeft
-                            style={{ marginLeft: "0.5rem", fontSize: "0.8rem" }}
-                        />
+                <Image
+                    src="/images/jiten.jpg"
+                    alt="Jiten Image"
+                    borderRadius="50%"
+                    boxShadow="0px 0px 10px 10px rgba(0,0,0,0.5)"
+                    width={{ base: "8rem", md: "12rem", lg: "17rem" }}
+                />
+                <Box>
+                    <Flex
+                        direction="column"
+                        textAlign={{ base: "center", md: "start" }}
+                    >
                         <Text
-                            color="#384765"
-                            fontSize={{ base: "2.5rem", md: "3rem" }}
+                            casing="uppercase"
+                            fontSize={{ base: "1rem", md: "1.5rem" }}
+                        >
+                            Hi!
+                        </Text>
+                        <Text
+                            casing="uppercase"
+                            fontSize={{ base: "1.5rem", md: "2rem" }}
                             fontWeight="bold"
                         >
-                            Jiten
-                        </Text>
-                        <FaQuoteRight
-                            style={{ marginLeft: "0.5rem", fontSize: "0.8rem" }}
-                        />
-                    </Flex>
-                </Text>
-                <Text
-                    casing="uppercase"
-                    fontSize={{ base: "3rem", md: "4rem" }}
-                    fontWeight="extrabold"
-                    lineHeight="1"
-                >
-                    FRONT END DEV
-                </Text>
-                <Flex gap="1rem" mt="1rem">
-                    <Link
-                        href="/cv/Resume.pdf"
-                        download
-                        _hover={{ "&::after": { content: "none" } }}
-                    >
-                        <Button
-                            variant="outline"
-                            size="lg"
-                            justifyContent={{ base: "center", md: "start" }}
-                            sx={btnStyle}
-                            rightIcon={
-                                <FaCloudDownloadAlt
-                                    style={{ fontSize: "1.5rem" }}
+                            <Flex justifyContent={{ base: "center", md: "start" }}>
+                                I&apos;m
+                                <FaQuoteLeft
+                                    style={{ marginLeft: "0.5rem", fontSize: "0.8rem" }}
                                 />
-                            }
-                        >
-                            Download CV
-                        </Button>
-                    </Link>
-                    <Link
-                        href="#contact"
-                        _hover={{ "&::after": { content: "none" } }}
-                    >
-                        <Button
-                            variant="outline"
-                            size="lg"
-                            sx={btnStyle}
-                            rightIcon={
-                                <FaHandshake style={{ fontSize: "1.5rem" }} />
-                            }
-                        >
-                            Hire Me
-                        </Button>
-                    </Link>
-                </Flex>
+                                <Text
+                                    color="#fff"
+                                    fontSize={{ base: "2.5rem", md: "3rem" }}
+                                    fontWeight="bold"
+                                >
+                                    Jiten
+                                </Text>
+                                <FaQuoteRight
+                                    style={{ marginLeft: "0.5rem", fontSize: "0.8rem" }}
+                                />
+                            </Flex>
+                        </Text>
+                        <Box>
+                            <Text
+                                casing="uppercase"
+                                fontSize={{ base: "3rem", md: "4rem" }}
+                                fontWeight="extrabold"
+                                lineHeight="1"
+                            >
+                                Front End Dev
+                            </Text>
+                        </Box>
+                        <Box>
+                            <Flex gap="1rem" mt="1rem" justifyContent={{ base: "center", md: "left" }}>
+                                <Link
+                                    href="/cv/Resume.pdf"
+                                    download
+                                    _hover={{ "&::after": { content: "none" } }}
+                                >
+                                    <Button
+                                        variant="outline"
+                                        size="lg"
+                                        justifyContent={{ base: "center", md: "start" }}
+                                        sx={btnStyle}
+                                        rightIcon={
+                                            <FaCloudDownloadAlt
+                                                style={{ fontSize: "1.5rem" }}
+                                            />
+                                        }
+                                    >
+                                        Download CV
+                                    </Button>
+                                </Link>
+                                <Link
+                                    href="#contact"
+                                    _hover={{ "&::after": { content: "none" } }}
+                                >
+                                    <Button
+                                        variant="outline"
+                                        size="lg"
+                                        sx={btnStyle}
+                                        rightIcon={
+                                            <FaHandshake style={{ fontSize: "1.5rem" }} />
+                                        }
+                                    >
+                                        Hire Me
+                                    </Button>
+                                </Link>
+                            </Flex>
+                        </Box>
+                    </Flex>
+                </Box>
             </Flex>
-        </Flex>
+        </>
     );
 };
 
