06bbefd29a535f597f60d4ae2f0df8411520ebd2
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-05 17:34:55 +0545

    feat(component and meta): Head component added and meta tag added
    - Head component from next/head added
    - meta tags added inside Head component for viewport configuration

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <meta key='viewport' name='viewport' content='width=device-width, initial-scale=1.0' />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
