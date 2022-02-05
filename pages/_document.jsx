38079fbb418c3572672fe5f4c7a138dabac4ae6d
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-05 17:36:47 +0545

    feat(_document.jsx): default _document.jsx modified
    - Html tag added with lang='en' props
    - meta tags like keywords, charSet, description, author and copyright added

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,19 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+export default class Document extends NextDocument {
   render() {
     return (
-      <Html>
+      <Html lang='en'>
         <Head>
+          <meta charSet='utf-8' />
+          <meta name='keywords' content='Portfolio, Portfolio Website, Jiten, zeetaen, Jiten Gurung' />
+          <meta name='description' content='Portfolio Website of Jiten Gurung' />
+          <meta name='author' content='Jiten Gurung, zeetaen.gurung@gmail.com' />
+          <meta name='copyright' content='Jiten Gurung' />
           <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Poppins:wght@300;400;500;600;700&family=Roboto+Mono:ital,wght@1,100&family=Roboto:wght@100;300;400;500;700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;0,900;1,300&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
-          <form action="https://formsubmit.co/zeetaen.gurung@gmail.com" method="POST"></form>
           <Main />
           <NextScript />
         </body>
@@ -21,5 +19,3 @@ class MyDocument extends Document {
     )
   }
 }
-
-export default MyDocument
