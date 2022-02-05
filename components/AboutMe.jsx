482f8c49246f4aba6fae1eeca33f60729747f2f3
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-05 17:05:19 +0545

    feat: Box component added to encapsulate every Flex component

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -71,45 +71,95 @@ const AboutMe = () => {
                 fontSize={{ base: "3rem", md: "4rem" }}
                 fontFamily="inherit"
             >
-                Who Am I ?
+                About Me
             </Heading>
             <Center>
-                <Divider width={{ base: "40%", md: "18%" }}  mb="0.5rem" />
+                <Divider width={{ base: "40%", md: "18%" }} mb="0.5rem" />
             </Center>
             <Center>
-                <Text>
-                    My Introduction
-                </Text>
+                <Text>My Introduction</Text>
             </Center>
-            <Text
-                textAlign="center"
-                fontSize={{ base: "1.2rem", md: "1.5rem" }}
-                p={{ base: "1rem 1.5rem", md: "2rem 6rem" }}
-            >
-                An Front-End Web Developing aficionado from{" "}
-                <Link
-                    href="https://en.wikipedia.org/wiki/Nepal"
-                    target="_blank"
-                    rel="noreferrer noopener nofollow"
-                    sx={linkStyle}
-                    fontSize={{ base: "1.2rem", md: "1.5rem" }}
-                >
-                    🇳🇵
-                </Link>{" "}
-                with an acute detail to hone the skills as required by the
-                project requirement. A Web technology ethusiast with a bit of UI
-                designing proficiency and immense interest to learn new tech stacks.
-            </Text>
-            {/* <Grid placeContent="center" mb="1rem">
-                <NextLink href="/">
-                    <Link href="/" _hover={{ "&::after": { content: "none" } }}>
-                        <Button sx={btnStyle} variant="outline" size="lg" rightIcon={<RiArrowGoBackFill />}>
-                            Go Back to Home Page
-                        </Button>
-                    </Link>
-                </NextLink>
-            </Grid> */}
-        </Flex>
+            <Box m="3rem 15rem">
+                <Flex gap="4rem" justifyContent="center" alignItems="center" >
+                    <Box flex="1">
+                        <Image 
+                            src="/images/jiten.jpg" 
+                            alt="Jiten Image"
+                            borderRadius="50%"
+                            boxShadow="0px 0px 10px 10px rgba(0,0,0,0.5)"
+                            width={{ base: "8rem", md: "12rem", lg: "17rem" }}
+                        />
+                    </Box>
+                    <Box flex="2">
+                        <Flex direction="column">
+                            <Flex gap="1rem" mb="2rem">
+                                <Wrap sx={ wrapperStyle }>
+                                    <Flex sx={ aboutMeFlexStyle } >
+                                        <FaGraduationCap style={ iconStyle }/>
+                                        <Text sx={ titleStyle }>
+                                            Education
+                                        </Text>
+                                        <Text sx={ subTitleStyle }>
+                                            BSc Hons
+                                        </Text>
+                                    </Flex>
+                                </Wrap>
+                                <Wrap sx={ wrapperStyle }>
+                                    <Flex sx={ aboutMeFlexStyle } >
+                                        <GiAchievement style={ iconStyle } />
+                                        <Text sx={ titleStyle }>
+                                            Experience
+                                        </Text>
+                                        <Text sx={ subTitleStyle }>
+                                            1+ years
+                                        </Text>
+                                    </Flex>
+                                </Wrap>
+                                <Wrap sx={ wrapperStyle } >
+                                    <Flex sx={ aboutMeFlexStyle }>
+                                        <RiSuitcaseFill style={ iconStyle }/>
+                                        <Text sx={ titleStyle }>
+                                            Completed
+                                        </Text>
+                                        <Text sx={ subTitleStyle }>
+                                            10+ projects
+                                        </Text>
+                                    </Flex>
+                                </Wrap>
+                            </Flex>
+                            <Text
+                                fontSize={{ base: "1.2rem", md: "1.2rem" }}
+                                mb="1rem"
+                            >
+                                A Front-End developing aficionado from{" "}
+                                <Link
+                                    href="https://en.wikipedia.org/wiki/Nepal"
+                                    target="_blank"
+                                    rel="noreferrer noopener nofollow"
+                                    sx={linkStyle}
+                                    fontSize={{ base: "1.2rem", md: "1.5rem" }}
+                                >
+                                    🇳🇵
+                                </Link>{" "}
+                                with an acute detail to hone the skills as required by the
+                                project requirements. A web technology ethusiast with a modest
+                                experience of UI designing proficiency and immense interest to
+                                build elegant as well as fully functioning applications.
+                            </Text>
+                            <Box mt="1rem">
+                                <NextLink href="/#contact">
+                                    <Link _hover={{ "&::after": { content: "none" } }}>
+                                        <Button sx={btnStyle} variant="outline" size="lg">
+                                            Contact Me
+                                        </Button>
+                                    </Link>
+                                </NextLink>
+                            </Box>
+                        </Flex>
+                    </Box>
+                </Flex>
+            </Box>
+        </Flex>                                                                                                                                                                                                                                 
     );
 };
 
