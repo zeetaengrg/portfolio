6ad082363db60a2b2bd8bac5c9d58c3d6f93a5d0
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-05 19:08:14 +0545

    feat(component): relative components added
    - NextLink added from next/link
    - Navbar and Footer component added to be rendered on error page
    - Error component added to be rendered in to error page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,9 +1,18 @@
-const Error = () => {
+import NextLink from "next/link";
+import Navbar from '../components/Navbar';
+import Error from '../components/Error';
+import Footer from '../components/Footer';
+
+const ErrorPage = () => {
     return (
         <>
-            
+            <Navbar />
+            <NextLink href="/404">
+                <Error />
+            </NextLink>
+            <Footer />
         </>
     );
 };
 
-export default Error;
+export default ErrorPage;
