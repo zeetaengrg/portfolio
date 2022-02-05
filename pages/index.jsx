1e3aea069e6d9c733d632901ac805c7955979ed3
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-05 17:39:25 +0545

    refactor

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Intro from "../components/Intro";
 import Navbar from "../components/Navbar";
+import Intro from "../components/Intro";
 import AboutMe from "../components/AboutMe";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
