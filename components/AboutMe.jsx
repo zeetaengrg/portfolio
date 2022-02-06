77c5bfee5366306a15b6c97d293f31845ae73234
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-06 22:11:08 +0545

    refactor(component): New components created and added
    - ContactMeBtn component created for Contact Me button.
    - AboutMeWrapper component created for three about me section wraps.
    - AboutMeText component created for About me text section.

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -5,59 +5,14 @@ import {
     Text,
     Link,
     Flex,
-    Box,
-    Button,
-    Wrap
+    Box
 } from "@chakra-ui/react";
-import { GiAchievement } from "react-icons/gi";
-import { FaGraduationCap } from "react-icons/fa";
-import { IoMdContact } from "react-icons/io";
-import { RiSuitcaseFill } from "react-icons/ri";
 import NextLink from "next/link";
+import ContactMeBtn from "./ContactMeBtn";
+import AboutMeWrapper from "./AboutMeWrapper";
+import AboutMeText from "./AboutMeText";
 
 const AboutMe = () => {
-
-    const btnStyle = {
-        padding: "0 0.8rem",
-        backgroundColor: "#16213E",
-        fontSize: "1.2rem",
-        _focus: { boxShadow: "none" },
-        _hover: { bg: "#a5abbd", color: "#384765", borderColor: "#384765" },
-        _active: { bg: "#a5abbd", color: "#384765", transform: "scale(0.98)" },
-    };
-
-    const linkStyle = {
-        _hover:{
-            "&::after": { content: "none" },
-        },
-    }
-
-    const wrapperStyle = {
-        bgColor: "#141E39",
-        borderRadius: "0.3rem",
-    }
-
-    const aboutMeFlexStyle = {
-        gap: "0.3rem",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: "1rem",
-    }
-
-    const iconStyle = {
-        color: "#0B111E",
-        fontSize: "1.5rem"
-    }
-
-    const titleStyle = {
-        color: "#a5abbd",
-        fontSize: "0.9rem"
-    }
-
-    const subTitleStyle = {
-        color: "#384765",
-        fontSize: "0.7rem"
-    }
 
     return (
         <Flex direction="column">
@@ -83,64 +38,13 @@ const AboutMe = () => {
                     <Flex
                         gap={{ base: "0.5rem", md: "1rem" }}
                     >
-                        <Wrap sx={wrapperStyle}>
-                            <Flex sx={aboutMeFlexStyle}>
-                                <FaGraduationCap style={iconStyle} />
-                                <Text sx={titleStyle}>Education</Text>
-                                <Text sx={subTitleStyle}>BSc Hons</Text>
-                            </Flex>
-                        </Wrap>
-                        <Wrap sx={wrapperStyle}>
-                            <Flex sx={aboutMeFlexStyle}>
-                                <GiAchievement style={iconStyle} />
-                                <Text sx={titleStyle}>Experience</Text>
-                                <Text sx={subTitleStyle}>1+ years</Text>
-                            </Flex>
-                        </Wrap>
-                        <Wrap sx={wrapperStyle}>
-                            <Flex sx={aboutMeFlexStyle}>
-                                <RiSuitcaseFill style={iconStyle} />
-                                <Text sx={titleStyle}>Completed</Text>
-                                <Text sx={subTitleStyle}>10+ projects</Text>
-                            </Flex>
-                        </Wrap>
+                        <AboutMeWrapper />
                     </Flex>
-                    <Text
-                        m={{ base: "1rem", md: "2rem" }}
-                        fontSize={{ base: "1.1rem", md: "1.3rem" }}
-                        textAlign="center"
-                    >
-                        A Front-End developing aficionado from{" "}
-                        <Link
-                            href="https://en.wikipedia.org/wiki/Nepal"
-                            target="_blank"
-                            rel="noreferrer noopener nofollow"
-                            sx={linkStyle}
-                            fontSize={{ base: "1.1rem", md: "1.5rem" }}
-                        >
-                            🇳🇵
-                        </Link>{" "}
-                        with an acute detail to hone the skills as required by
-                        the project requirements. A web technology ethusiast
-                        with a modest experience of UI designing proficiency and
-                        immense interest to build elegant as well as fully
-                        functioning applications.
-                    </Text>
+                    <AboutMeText />
                     <Box>
                         <NextLink href="/#contact">
                             <Link _hover={{ "&::after": { content: "none" } }}>
-                                <Button
-                                    sx={btnStyle}
-                                    variant="outline"
-                                    size="lg"
-                                    rightIcon={
-                                        <IoMdContact
-                                            style={{ fontSize: "1.5rem" }}
-                                        />
-                                    }
-                                >
-                                    Contact Me
-                                </Button>
+                                <ContactMeBtn />
                             </Link>
                         </NextLink>
                     </Box>
