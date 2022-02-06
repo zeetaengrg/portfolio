c162b090b4ab79ffa00ce6a37d5955fda9ec31d4
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-06 22:12:49 +0545

    refactor(component): Button component styling modified

--- components/DownloadBtn.jsx
+++ components/DownloadBtn.jsx
@@ -7,6 +7,10 @@ const DownloadBtn = () => {
         padding: "0 0.8rem",
         backgroundColor: "#141E39",
         fontSize: "1.2rem",
+        justifyContent: {
+            base: "center",
+            md: "start"
+        },
         _focus: { boxShadow: "none" },
         _hover: { bg: "#a5abbd", color: "#384765", borderColor: "#384765" },
         _active: { bg: "#a5abbd", color: "#384765", transform: "scale(0.98)" },
@@ -17,10 +21,6 @@ const DownloadBtn = () => {
             <Button
                 variant="outline"
                 size="lg"
-                justifyContent={{
-                    base: "center",
-                    md: "start",
-                }}
                 sx={btnStyle}
                 isLoading={false}
                 loadingText="Downloading"
