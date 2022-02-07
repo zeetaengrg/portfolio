4b3cba7ecbc24143da4d5f25641f42cccdc5b632
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-07 14:50:50 +0545

    refactor(components): Btn folder created
    - Btn folder created inside elements components folder.
    - this folder contains Button components like ContactMeBtn, HireMeBtn, etc.

---
+++ components/elements/Btn/DownloadBtn/DownloadBtn.jsx
@@ -0,0 +1,40 @@
+import { Button } from "@chakra-ui/react";
+import { FaCloudDownloadAlt } from "react-icons/fa";
+
+const DownloadBtn = () => {
+
+    const btnStyle = {
+        padding: "0 0.8rem",
+        backgroundColor: "#141E39",
+        fontSize: "1.2rem",
+        justifyContent: {
+            base: "center",
+            md: "start"
+        },
+        _focus: { boxShadow: "none" },
+        _hover: { bg: "#a5abbd", color: "#384765", borderColor: "#384765" },
+        _active: { bg: "#a5abbd", color: "#384765", transform: "scale(0.98)" },
+    };
+
+    return (
+        <>
+            <Button
+                variant="outline"
+                size="lg"
+                sx={btnStyle}
+                isLoading={false}
+                loadingText="Downloading"
+                spinnerPlacement="end"
+                rightIcon={
+                    <FaCloudDownloadAlt
+                        style={{ fontSize: "1.5rem" }}
+                    />
+                }
+            >
+                Download CV
+            </Button>
+        </>
+    );
+};
+
+export default DownloadBtn;
