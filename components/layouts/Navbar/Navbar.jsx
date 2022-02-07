d46d574e3ae5d86198b289de92f37ba626c90329
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-07 16:11:10 +0545

    refactor(component): Image component removed

--- components/layouts/Navbar/Navbar.jsx
+++ components/layouts/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Image, Link, Spacer } from "@chakra-ui/react";
+import { Box, Flex, Link, Spacer } from "@chakra-ui/react";
 import NextLink from "next/link";
 import LogoImage from "../../elements/LogoImage/LogoImage";
 
