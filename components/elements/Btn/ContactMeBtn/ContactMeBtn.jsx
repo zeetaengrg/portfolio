558dfbd9ee9abe19b4cc75c46353359843066e39
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-08 18:51:38 +0545

    style(button): scale transform modified

--- components/elements/Btn/ContactMeBtn/ContactMeBtn.jsx
+++ components/elements/Btn/ContactMeBtn/ContactMeBtn.jsx
@@ -9,7 +9,7 @@ const ContactMeBtn = () => {
         fontSize: "1.2rem",
         _focus: { boxShadow: "none" },
         _hover: { bg: "#a5abbd", color: "#384765", borderColor: "#384765" },
-        _active: { bg: "#a5abbd", color: "#384765", transform: "scale(0.98)" },
+        _active: { bg: "#a5abbd", color: "#384765", transform: "scale(0.99)" },
     };
 
     return (
--- components/elements/Btn/DownloadBtn/DownloadBtn.jsx
+++ components/elements/Btn/DownloadBtn/DownloadBtn.jsx
@@ -13,7 +13,7 @@ const DownloadBtn = () => {
         },
         _focus: { boxShadow: "none" },
         _hover: { bg: "#a5abbd", color: "#384765", borderColor: "#384765" },
-        _active: { bg: "#a5abbd", color: "#384765", transform: "scale(0.98)" },
+        _active: { bg: "#a5abbd", color: "#384765", transform: "scale(0.99)" },
     };
 
     return (
