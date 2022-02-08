8af278d11324bf8438159ef281c0b28c410761f6
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-08 21:03:55 +0545

    refactor: packages added

--- components/modules/ContactForm/ContactForm.jsx
+++ components/modules/ContactForm/ContactForm.jsx
@@ -1,5 +1,7 @@
-import { VStack, FormLabel, Input, Textarea, Button } from '@chakra-ui/react';
+import { VStack, FormLabel, Input, Textarea, Button, FormControl } from '@chakra-ui/react';
 import { RiSendPlaneFill } from "react-icons/ri";
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 
 const ContactForm = () => {
 
@@ -20,53 +22,101 @@ const ContactForm = () => {
         _active: { bg: "#a5abbd", color: "#384765", transform: "scale(0.99)" },
     };
 
+    const initialValues = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        message: ""
+    };
+    
+    const validationSchema = Yup.object().shape({
+        firstName: Yup.string()
+            .min(3, 'Too Short!')
+            .max(50, 'Too Long!')
+            .required('Required'),
+        lastName: Yup.string()
+            .min(3, 'Too Short!')
+            .max(50, 'Too Long!')
+            .required('Required'),
+        email: Yup.string()
+            .email('Please Enter Valid Email')
+            .required('Required'),
+        message: Yup.string()
+            .min(100, 'Too Short!')
+            .max(2000, 'Too Long!')
+            .required('Required'),
+    })
+    
+    const onSubmit = (values, actions) => {
+        setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            actions.resetForm();
+            actions.setSubmitting(false);
+        }, 2000)
+    };
+
     return (
-        <VStack
-            spacing={2}
-            alignItems="flex-start"
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
         >
-            <FormLabel mb="0rem">First Name</FormLabel>
-            <Input
-                sx={inputStyle}
-                type="text"
-                placeholder="Insert Your First Name..."
-                name="firstName"
-                required
-            />
-            <FormLabel>Last Name</FormLabel>
-            <Input
-                sx={inputStyle}
-                type="text"
-                placeholder="Insert Your Last Name..."
-                name="lastName"
-                required
-            />
-            <FormLabel>Email Address</FormLabel>
-            <Input
-                sx={inputStyle}
-                placeholder="Insert Your Email..."
-                name="email"
-                type="email"
-                required
-            />
-            <FormLabel>Message</FormLabel>
-            <Textarea
-                sx={inputStyle}
-                placeholder="Insert Your Message..."
-                name="message"
-                height="10rem"
-                required
-            />
-            <Button
-                mt="1rem"
-                variant="outline"
-                sx={btnStyle}
-                type="submit"
-                rightIcon={<RiSendPlaneFill style={{ fontSize: "1.5rem" }}/>}
-            >
-                Send
-            </Button>
-        </VStack>
+            {(props) => (
+                <Form>
+                    <VStack spacing={2} alignItems="flex-start">
+                        <FormLabel htmlFor="firstName">First Name</FormLabel>
+                        <Input
+                            sx={inputStyle}
+                            type="text"
+                            placeholder="Insert Your First Name..."
+                            id="firstName"
+                            name="firstName"
+                            required
+                        />
+                        <FormLabel htmlFor="lastName">Last Name</FormLabel>
+                        <Input
+                            sx={inputStyle}
+                            type="text"
+                            placeholder="Insert Your Last Name..."
+                            id="lastName"
+                            name="lastName"
+                            required
+                        />
+                        <FormLabel htmlFor="email">Email Address</FormLabel>
+                        <Input
+                            sx={inputStyle}
+                            placeholder="Insert Your Email..."
+                            name="email"
+                            id="email"
+                            type="email"
+                            required
+                        />
+                        <FormLabel htmlFor="message">Message</FormLabel>
+                        <Textarea
+                            sx={inputStyle}
+                            placeholder="Insert Your Message..."
+                            name="message"
+                            id="message"
+                            height="10rem"
+                            required
+                        />
+                        <Button
+                            mt="1rem"
+                            variant="outline"
+                            sx={btnStyle}
+                            type="submit"
+                            isLoading={props.isSubmitting}
+                            loadingText="Sending..."
+                            rightIcon={
+                                <RiSendPlaneFill style={{ fontSize: "1.5rem" }} />
+                            }
+                        >
+                            Send
+                        </Button>
+                    </VStack>
+                </Form>
+            )}
+        </Formik>
     );
 };
 
