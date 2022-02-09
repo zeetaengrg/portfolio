8c8ec624e70020ef418daaf0b99b753d3db25c5d
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-09 11:12:01 +0545

    feat(logo): logo image updated

--- components/layouts/Footer/Footer.jsx
+++ components/layouts/Footer/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
         <Box>
             <Flex bgColor="#141E39" justifyContent="center" direction="column">
                 <Center>
-                    <Image src="/images/logo.webp" width="69px" height="45px" alt="logo" m={{ base: "1rem 0", md: "2rem 0" }} />
+                    <Image src="/images/logo.png" width="69px" height="45px" alt="Logo" m={{ base: "1rem 0", md: "2rem 0" }} />
                 </Center>
-                <Flex m={{ base: "1rem 0", md: "0.5rem 0" }} gap="2rem" justifyContent="center">
+                <Flex m={{ base: "1rem 0", md: "0rem 0" }} gap="2rem" justifyContent="center">
                     <SocialIconLinks />
                 </Flex>
                 <Text fontSize="0.8rem" m={{ base: "0.5rem", md: "1rem" }} >
