fb741d52bc9da3006852437720d10f927fa47fe9
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-12 22:15:16 +0545

    style(padding): padding in aboutme section modified

--- components/layouts/AboutMe/AboutMe.jsx
+++ components/layouts/AboutMe/AboutMe.jsx
@@ -33,7 +33,7 @@ const AboutMe = () => {
             <Center>
                 <Text fontWeight="bold" fontSize="1.1rem">My Introduction</Text>
             </Center>
-            <Box m={{ base: "1.5rem 2rem", md: "3rem 5rem", lg: "3rem 10rem" }}>
+            <Box p={{ base: "1.5rem", md: "3rem 5rem", lg: "3rem 10rem" }}>
                 <Flex direction="column" alignItems="center">
                     <Flex
                         gap={{ base: "0.5rem", md: "1rem" }}
