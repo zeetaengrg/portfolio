7bf7e06fcea5ed54323869e531f940c986234da3
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-13 15:58:13 +0545

    style(framer-motion): motion added to logo image

--- components/elements/LogoImage/LogoImage.jsx
+++ components/elements/LogoImage/LogoImage.jsx
@@ -1,17 +1,45 @@
 import NextLink from "next/link";
 import { Link, Image } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+
+const MotionImage = motion(Image)
+
+const imageVariants = {
+    hidden: {
+        opacity: 0,
+        x: -80,
+    },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            delay: 0.1,
+            duration: 1,
+            type: "spring",
+            stiffness: 120
+        }
+    }
+}
 
 const LogoImage = () => {
 
     const linkStyle = {
         _focus: { boxShadow: "none" },
-    }
+    };
 
     return (
         <>
             <NextLink href="/" passHref>
                 <Link sx={linkStyle}>
-                    <Image src="/images/logo.webp" alt="Logo" width="69px" height="45px" />
+                    <MotionImage
+                        src="/images/logo.svg"
+                        alt="Logo"
+                        width="60px"
+                        height="45px"
+                        variants={imageVariants}
+                        initial="hidden"
+                        animate="visible"
+                    />
                 </Link>
             </NextLink>
         </>
