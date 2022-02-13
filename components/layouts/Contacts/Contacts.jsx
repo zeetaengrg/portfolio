f6ff8ec9904c5ce95f12f3c4b2d92339beba202f
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-13 22:57:46 +0545

    style(styled object): styled objects created for each components for code readability

--- components/layouts/Contacts/Contacts.jsx
+++ components/layouts/Contacts/Contacts.jsx
@@ -12,47 +12,56 @@ import ContactInfo from "../../modules/ContactInfo/ContactInfo";
 
 const Contacts = () => {
 
+    const headingStyle = {
+        id: "contacts",
+        textAlign: "center",
+        textTransform: "uppercase",
+        bgGradient: "linear(to-r, #fff, #a5abbd, #384765, #152641, #0B111E)",
+        bgClip: "text",
+        fontFamily: "inherit",
+        fontSize: { base: "3rem", md: "4rem" },
+        mt: { base: "3rem", md: "5rem" },
+    }
+
+    const imgStyle = {
+        display: {
+            base: "none",
+            md: "block"
+        },
+        position: "absolute",
+        bottom: "5rem",
+        width: "50rem",
+        opacity: "0.2",
+    }
+
+    const flexStyle = {
+        p: { base: "1.5rem", md: "2rem 0rem" },
+        gap: { base: "2rem" },
+        justifyContent: "space-around",
+        alignItems: "center",
+        flexDirection: { base: "column", md: "row" },
+        position: "relative",
+    }
+
     return (
         <Box position="relative">
-            <Heading
-                id="contact"
-                textAlign="center"
-                textTransform="uppercase"
-                bgGradient="linear(to-r, #fff, #a5abbd, #384765, #152641, #0B111E)"
-                bgClip="text"
-                fontSize={{ base: "3rem", md: "4rem" }}
-                fontFamily="inherit"
-                mt={{ base: "3rem", md: "5rem" }}
-            >
-                Get In Touch
-            </Heading>
+            <Heading sx={headingStyle}>Get In Touch</Heading>
             <Center>
                 <Divider width={{ base: "60%", md: "25%" }} mb="0.5rem" />
             </Center>
             <Center>
-                <Text fontWeight="bold" fontSize="1.1rem">Contact Me</Text>
+                <Text fontWeight="bold" fontSize="1.1rem">
+                    Contact Me
+                </Text>
             </Center>
             <Center>
                 <Image
-                    display={{ base: "none", md: "block" }}
                     src="/images/contact.webp"
                     alt="Contact Me SVG"
-                    sx={{
-                        position: "absolute",
-                        bottom: "4rem",
-                        width: "50rem",
-                        opacity: "0.2",
-                    }}
+                    sx={imgStyle}
                 />
             </Center>
-            <Flex
-                p={{ base: "1.5rem", md: "2rem 0rem" }}
-                gap={{ base: "2rem" }}
-                justifyContent="space-around"
-                alignItems="center"
-                direction={{ base: "column", md: "row" }}
-                position="relative"
-            >
+            <Flex sx={flexStyle}>
                 <ContactForm />
                 <ContactInfo />
             </Flex>
