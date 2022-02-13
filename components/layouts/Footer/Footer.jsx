930b4b5dc4d10725493be8f793de570c6365c779
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-13 22:58:54 +0545

    style(styled object): styled object created for readability

--- components/layouts/Footer/Footer.jsx
+++ components/layouts/Footer/Footer.jsx
@@ -4,18 +4,35 @@ import SocialIconLinks from '../../elements/SocialIconLinks/SocialIconLinks';
 
 const Footer = () => {
 
+    const imageStyle = {
+        width: "60px",
+        height: "45px",
+        m: {
+            base: "1rem 0", md: "2rem 0"
+        }
+    }
+
     return (
         <Box>
             <Flex bgColor="#141E39" justifyContent="center" direction="column">
                 <Center>
-                    <Image src="/images/logo.svg" width="60px" height="45px" alt="Logo" m={{ base: "1rem 0", md: "2rem 0" }} />
+                    <Image src="/images/logo.svg" alt="Logo" sx={imageStyle} />
                 </Center>
-                <Flex m={{ base: "1rem 0", md: "0rem 0" }} gap="2rem" justifyContent="center">
+                <Flex
+                    m={{ base: "1rem 0", md: "0rem 0" }}
+                    gap="2rem"
+                    justifyContent="center"
+                >
                     <SocialIconLinks />
                 </Flex>
-                <Text fontSize="0.8rem" m={{ base: "0.5rem", md: "1rem" }} >
+                <Text
+                    fontSize="0.8rem"
+                    mt={{ base: "0.5rem", md: "1rem" }}
+                    mb="0.5rem"
+                >
                     <Flex justifyContent="center" alignItems="center">
-                        2022 <FaCopyright style={{margin: "0 0.5rem"}} />Jiten Gurung. All Rights Reserved.
+                        2022 <FaCopyright style={{ margin: "0 0.5rem" }} />
+                        Jiten Gurung. All Rights Reserved.
                     </Flex>
                 </Text>
             </Flex>
