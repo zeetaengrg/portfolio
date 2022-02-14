1ee949edc5784fe72ce1145c5b964bcd15cde1a2
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-14 22:41:32 +0545

    feat(data): separate projects-data created

--- components/layouts/Projects/Projects.jsx
+++ components/layouts/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import { Center, Divider, Flex, Grid, Heading, Image, Text, Link, Box } from "@chakra-ui/react";
-import { projects } from "../../../data/data";
+import { projects } from "../../../data/projects-data";
 
 const Projects = () => {
 
