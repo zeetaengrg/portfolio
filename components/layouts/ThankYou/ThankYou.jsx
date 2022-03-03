82f671f823d297183d33a9cff6e26ba66c357207
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-03 23:22:44 +0545

    refactor

--- components/layouts/ThankYou/ThankYou.jsx
+++ components/layouts/ThankYou/ThankYou.jsx
@@ -1,53 +1,9 @@
 import { Box, Divider, Flex, Heading, Text } from "@chakra-ui/react";
 import { FaPrayingHands } from "react-icons/fa";
 import { GoBackBtn } from "../../elements";
+import { flexStyle, headingStyle, textStyle, boxStyle } from "./Styles";
 
 const ThankYou = () => {
-
-    const flexStyle = {
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        height: {
-            base: "68.9vh",
-            md: "66.5vh",
-            lg: "66.5vh"
-        },
-    }
-    
-    const headingStyle = {
-        color: "#0B111E",
-        fontWeight: "700",
-        textTransform: "uppercase",
-    }
-
-    const boxStyle = {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        background: "#384765",
-        width: {
-            base: "80%",
-            md: "40%"
-        },
-        padding: {
-            base: "0.5rem",
-            md: "1.5rem"
-        },
-        borderRadius: "0.5rem",
-        margin: {
-            base: "1rem 0",
-            md: "1.5rem 0"
-        }
-    }
-
-    const textStyle = {
-        fontSize: "1.2rem",
-        color: "#fff",
-        margin: "1rem",
-        textAlign: "center",
-    }
 
     return (
         <>
