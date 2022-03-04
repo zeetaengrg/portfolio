5d44d8c52d1a0a508803c264815ca04a4a59cd26
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-04 12:51:57 +0545

    feat(styled component): new component created which contains styled objects

--- components/layouts/Projects/Projects.jsx
+++ components/layouts/Projects/Projects.jsx
@@ -3,49 +3,11 @@ import { projects } from "../../../data/projects-data";
 import { motion } from "framer-motion";
 import { Header, LineDivider, SubHeader } from "../../elements";
 import { linkStyle } from "../../elements/LinkStyle/LinkStyle";
+import { commonVariants, flexStyle, imgStyle, techTextStyle } from "./ProjectsStyle";
 
 const MotionFlex = motion(Flex)
 
-const commonVariants = {
-    hidden: {
-        opacity: 0,
-        y: "5vh",
-    },
-    visible: {
-        opacity: 1,
-        y: 0,
-    }
-}
-
 const Projects = () => {
-
-    const flexStyle = {
-        margin: {
-            base: "1.5rem 2rem",
-            md: "4rem 6rem",
-        },
-        gap: {
-            base: "1.5rem",
-            md: "2rem",
-        },
-        justifyContent: "center",
-    }
-
-    const imgStyle = {
-        transition: "all 0.3s ease-in-out",
-        _hover: {
-            transform: "scale(1.05)",
-            transition: "all 0.3s ease-in-out"
-        },
-        borderRadius: "0.5rem",
-        boxShadow: "0 0.5rem 1.5rem 1rem rgba(0,0,0,0.5)",
-    };
-
-    const techTextStyle = {
-        color: "#ecb7d4",
-        mt: "0.5rem",
-        textTransform: "uppercase",
-    }
 
     return (
         <Box>
