5a0ced18a33828185f3451dd5935fc0e0d425706
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-04 12:53:54 +0545

    feat(styled object component): new component created which contains styled objects

---
+++ components/elements/AboutMeWrapper/AboutMeWrapperStyle.js
@@ -0,0 +1,28 @@
+export const listItemStyle = {
+    width: "7rem",
+    bgColor: "#141E39",
+    borderRadius: "0.3rem",
+    padding: "0.8rem",
+    display: "flex",
+    gap: "0.5rem",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    boxShadow: "4px 4px 8px rgba(0, 0, 0, 0.27), 0 0 40px rgba(0, 0, 0, 0.06) inset",
+}
+
+export const iconStyle = {
+    color: "#0B111E",
+    fontSize: "1.5rem",
+    marginInlineEnd: "0rem"
+}
+
+export const titleStyle = {
+    color: "#a5abbd",
+    fontSize: "0.8rem",
+}
+
+export const subTitleStyle = {
+    color: "#3892f9",
+    fontSize: "0.7rem"
+}
---
+++ components/layouts/Projects/ProjectsStyle.js
@@ -0,0 +1,38 @@
+export const commonVariants = {
+    hidden: {
+        opacity: 0,
+        y: "5vh",
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+    }
+}
+
+export const flexStyle = {
+    margin: {
+        base: "1.5rem 2rem",
+        md: "4rem 6rem",
+    },
+    gap: {
+        base: "1.5rem",
+        md: "2rem",
+    },
+    justifyContent: "center",
+}
+
+export const imgStyle = {
+    transition: "all 0.3s ease-in-out",
+    _hover: {
+        transform: "scale(1.05)",
+        transition: "all 0.3s ease-in-out"
+    },
+    borderRadius: "0.5rem",
+    boxShadow: "0 0.5rem 1.5rem 1rem rgba(0,0,0,0.5)",
+};
+
+export const techTextStyle = {
+    color: "#ecb7d4",
+    mt: "0.5rem",
+    textTransform: "uppercase",
+}
