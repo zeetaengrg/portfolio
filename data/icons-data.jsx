3162b639f7e023cf8ca1a43f70396223166a4133
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-08 23:05:59 +0545

    feat(images): svg images used instead of png

--- data/icons-data.jsx
+++ data/icons-data.jsx
@@ -1,104 +1,105 @@
 export const icons = [
     {
-        frontend: [
+        language: [
             {
                 id: 1,
                 name: "HTML5",
-                image: "/images/html.png",
+                image: "/images/html.svg",
             },
             {
                 id: 2,
                 name: "CSS3",
-                image: "/images/css.png",
+                image: "/images/css.svg",
             },
             {
                 id: 3,
                 name: "JavaScript",
-                image: "/images/javascript.png",
+                image: "/images/javascript.svg",
             },
-            {
-                id: 4,
-                name: "ReactJS",
-                image: "/images/reactjs.png",
-            },
-            {
-                id: 5,
-                name: "NextJS",
-                image: "/images/nextjs.png",
-            }
         ],
         backend: [
             {
                 id: 1,
                 name: "NodeJS",
-                image: "/images/nodejs.webp",
+                image: "/images/nodejs.svg",
             },
             {
                 id: 2,
                 name: "ExpressJS",
-                image: "/images/expressjs.png",
+                image: "/images/expressjs.svg",
             }
         ],
         database: [
             {
                 id: 1,
                 name: "MongoDB",
-                image: "/images/mongodb.png",
+                image: "/images/mongodb.svg",
             }
         ],
-        ui: [
+        library: [
             {
                 id: 1,
-                name: "Bootstrap",
-                image: "/images/bootstrap.png",
+                name: "ReactJS",
+                image: "/images/reactjs.svg",
             },
             {
                 id: 2,
-                name: "Tailwind CSS",
-                image: "/images/tailwind.png",
-            },
-            {
-                id: 3,
-                name: "Material-UI",
-                image: "/images/materialui.png",
+                name: "NextJS",
+                image: "/images/nextjs.png",
             },
             {
                 id: 4,
                 name: "Chakra-UI",
-                image: "/images/chakraui.webp",
+                image: "/images/chakraui.svg",
             },
             {
                 id: 5,
-                name: "Figma",
-                image: "/images/figma.png",
+                name: "Material-UI",
+                image: "/images/mui.svg",
             },
             {
                 id: 6,
+                name: "Bootstrap",
+                image: "/images/bootstrap.svg",
+            },
+            {
+                id: 7,
+                name: "TailwindCSS",
+                image: "/images/tailwindcss.svg",
+            },
+            {
+                id: 8,
                 name: "Framer Motion",
                 image: "/images/framer-motion.png",
             }
+            
         ],
         tools: [
             {
                 id: 1,
                 name: "Sass",
-                image: "/images/sass.png",
+                image: "/images/sass.svg",
             },
             {
                 id: 2,
                 name: "Styled Components",
-                image: "/images/styled-components.png",
+                image: "/images/styled-components.svg",
             },
             {
                 id: 3,
                 name: "Git",
-                image: "/images/git.png",
+                image: "/images/git.svg",
             },
             {
                 id: 4,
                 name: "GitHub",
-                image: "/images/github.png",
-            }
+                image: "/images/github.svg",
+            },
+            {
+                id: 5,
+                name: "Figma",
+                image: "/images/figma.svg",
+            },
         ]
     }
 ]
--- data/projects-data.jsx
+++ data/projects-data.jsx
@@ -4,9 +4,9 @@ export const projects = [
         titleOne: "Hulu Landing Page",
         titleTwo: "Portfolio Website",
         descriptionOne:
-            "A fully responsive design clone of Hulu's Landing Page with a little bit of Javascript for 'Login' modal functionality. One of my early learning phase project and having learned how to build Netflix clone from Traversy Media, I put my skills to test and built it from scratch with pixel perfect design of Hulu's Website. Here, I learned Media Queries and JavaScript DOM manipulation techniques.",
+            "A fully responsive design clone of Hulu's Landing Page with a little bit of Javascript for 'Login' modal functionality. Built it from scratch with pixel perfect design of Hulu's Website to put the skills to test. Learned Media Queries and JavaScript DOM manipulation techniques.",
         descriptionTwo:
-            "My portfolio website which showcases my skills, the tools and technologies I use, projects I have worked or been working and a simple way to contact me. Having learned how to build Portfolio website from Kevin Powell using HTML, CSS and JavaScript, to test myself and also to build my own Portfolio Website, I built it from scratch to implement my NextJS skills.",
+            "A personal portfolio website which showcases the skills, tools and technologies used and recent projects. Having learned how to build Portfolio website from Kevin Powell using HTML, CSS and JavaScript, to put the skills to test and also to build personal Portfolio Website, it was built from scratch to implement NextJS framework.",
         imageOne: "/images/hulu.webp",
         altOne: "Hulu Landing Page",
         linkOne: "https://hulu-landing-page-clone.vercel.app/",
@@ -17,17 +17,17 @@ export const projects = [
             {
                 id: 1,
                 name: "HTML",
-                image: "/images/html.png",
+                image: "/images/html.svg",
             },
             {
                 id: 2,
                 name: "CSS",
-                image: "/images/css.png",
+                image: "/images/css.svg",
             },
             {
                 id: 3,
                 name: "JavaScript",
-                image: "/images/javascript.png",
+                image: "/images/javascript.svg",
             }
         ],
         technologyTwo: [
@@ -39,7 +39,12 @@ export const projects = [
             {
                 id: 2,
                 name: "Chakra-UI",
-                image: "/images/chakraui.webp",
+                image: "/images/chakraui.svg",
+            },
+            {
+                id: 3,
+                name: "Framer Motion",
+                image: "/images/framer-motion.png",
             }
         ]
     },
@@ -50,7 +55,7 @@ export const projects = [
         descriptionOne:
             `This project shows a simple demonstration of how to fetch the data from the external API. In this project, Axios is used to fetch the data from "The Breaking Bad API" (https://breakingbadapi.com/).`,
         descriptionTwo:
-            "This is a clothing store e-commerce website which provides a platform to sell clothing products. This project is under construction.",
+            "This is a clothing store e-commerce website built to demonstrate full stack development technologies. This project is under construction.",
         imageOne: "/images/breakingbad.webp",
         altOne: "Breaking Bad API",
         linkOne: "https://reverent-morse-0a844d.netlify.app/",
@@ -61,29 +66,39 @@ export const projects = [
             {
                 id: 1,
                 name: "React",
-                image: "/images/reactjs.png",
+                image: "/images/reactjs.svg",
             },
             {
                 id: 2,
                 name: "Sass",
-                image: "/images/sass.png",
+                image: "/images/sass.svg",
             },
         ],
         technologyTwo: [
             {
                 id: 1,
                 name: "ReactJS",
-                image: "/images/reactjs.png",
+                image: "/images/reactjs.svg",
             },
             {
                 id: 2,
                 name: "Styled-Components",
-                image: "/images/styled-components.png",
+                image: "/images/styled-components.svg",
             },
             {
                 id: 3,
-                name: "Materail-UI",
-                image: "/images/materialui.png",
+                name: "NodeJS",
+                image: "/images/nodejs.svg",
+            },
+            {
+                id: 4,
+                name: "ExpressJS",
+                image: "/images/expressjs.svg",
+            },
+            {
+                id: 5,
+                name: "MongoDB",
+                image: "/images/mongodb.svg",
             }
         ]
     },
