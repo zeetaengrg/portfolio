b4d39b4d9ec4e84c75fc7bea77dbe1c1410263db
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-09 20:26:55 +0545

    fix(responsive): footer zIndex issue fixed

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,18 +1,49 @@
+import { Box } from "@chakra-ui/react";
 import Head from "next/head";
 import NextLink from "next/link";
 import { Navbar, Error, Footer } from "../components/layouts";
 
 const ErrorPage = () => {
+
+    const heroBoxStyle = {
+        position: {
+            base: "initial",
+            md: "relative",
+        },
+        zIndex: {
+            base: "auto",
+            md: "999",
+        },
+        height: {
+            base: "100%",
+        }
+    }
+
+    const footerBoxStyle = {
+        position: {
+            base: "initial",
+            md: "absolute",
+        },
+        width: "100%",
+        // bottom: {
+        //     base: "10%",
+        // }
+    }
+
     return (
         <>
             <Head>
                 <title>404 Error: Page Not Found</title>
             </Head>
-            <Navbar />
-            <NextLink href="/404">
-                <Error />
-            </NextLink>
-            <Footer />
+            <Box sx={heroBoxStyle}>
+                <Navbar />
+                <NextLink href="/404">
+                    <Error />
+                </NextLink>
+            </Box>
+            <Box sx={footerBoxStyle}>
+                <Footer />
+            </Box>
         </>
     );
 };
