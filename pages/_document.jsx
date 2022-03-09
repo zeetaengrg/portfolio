79c74e89d725994b8bddb08e164409e8273f8e80
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-09 20:28:02 +0545

    fix(meta): author name updated in meta tag

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,7 +7,7 @@ export default class Document extends NextDocument {
           <meta charSet='utf-8' />
           <meta name='keywords' content='Portfolio, Portfolio Website, Jiten, zeetaen, Jiten Gurung, Jitendra, Jitendra Kumar Gurung' />
           <meta name='description' content='Portfolio Website of Jiten Gurung' />
-          <meta name='author' content='Jiten Gurung, zeetaen.gurung@gmail.com' />
+          <meta name='author' content='Jitendra Gurung, Jiten Gurung' />
           <meta name='copyright' content='Jiten Gurung' />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"></link>
