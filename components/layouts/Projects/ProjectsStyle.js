a59bc4c461ef2e6a48a4e84590c9cb5264f76614
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-09 14:26:44 +0545

    style(components): All the compoents in Projects component styled

--- components/layouts/Projects/ProjectsStyle.js
+++ components/layouts/Projects/ProjectsStyle.js
@@ -21,18 +21,44 @@ export const flexStyle = {
     justifyContent: "center",
 }
 
+export const boxStyle = {
+    flex: "2",
+}
+
 export const imgStyle = {
-    transition: "all 0.3s ease-in-out",
-    _hover: {
-        transform: "scale(1.05)",
-        transition: "all 0.3s ease-in-out"
-    },
     borderRadius: "0.5rem",
     boxShadow: "0 0.5rem 1.5rem 1rem rgba(0,0,0,0.5)",
 };
+
+export const linkContainer = {
+    display: "flex",
+    justifyContent: {
+        base: "center",
+        md: "flex-start",
+    },
+    gap: "1rem",
+    marginTop: "0.5rem",
+}
+
+export const linkToProjects = {
+    backgroundColor: "#141E39",
+    padding: "0.3rem 0.4rem",
+    borderRadius: "0.3rem",
+    _hover: {
+        "&::after": { content: "none" },
+        backgroundColor: "#a5abbd",
+        color: "#0B111E",
+    },
+    _focus: { boxShadow: "none" },
+    boxShadow: "4px 4px 8px rgba(0, 0, 0, 0.27), 0 0 40px rgba(0, 0, 0, 0.06) inset",
+}
 
 export const techTextStyle = {
     color: "#ecb7d4",
     mt: "0.5rem",
     textTransform: "uppercase",
+    textAlign: {
+        base: "center",
+        md: "left",
+    }
 }
