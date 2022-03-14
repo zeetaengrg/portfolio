345492a02c813cfcafcd0584c1120ec75e684807
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-14 23:14:31 +0545

    feat(directory): folder structure update

--- components/layouts/index.js
+++ components/layouts/index.js
@@ -1,9 +1,2 @@
-export { default as AboutMe } from "./AboutMe/AboutMe";
-export { default as Contacts } from "./Contacts/Contacts";
 export { default as Error } from "./Error/Error";
-export { default as Footer } from "./Footer/Footer";
-export { default as Hero } from "./Hero/Hero";
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as Projects } from "./Projects/Projects";
-export { default as Skills } from "./Skills/Skills";
 export { default as ThankYou } from "./ThankYou/ThankYou";
