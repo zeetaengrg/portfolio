15ac2cd62b024213ec5b1dc357823af967a788ba
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-17 23:33:41 +0545

    fix(array): icons data array issue fixed

--- data/icons-data.js
+++ data/icons-data.js
@@ -1,104 +1,100 @@
-export const icons = [
+export const language = [
     {
-        language: [
-            {
-                id: 1,
-                name: "HTML5",
-                image: "/images/html.svg",
-            },
-            {
-                id: 2,
-                name: "CSS3",
-                image: "/images/css.svg",
-            },
-            {
-                id: 3,
-                name: "JavaScript",
-                image: "/images/javascript.svg",
-            },
-        ],
-        backend: [
-            {
-                id: 1,
-                name: "NodeJS",
-                image: "/images/nodejs.svg",
-            },
-            {
-                id: 2,
-                name: "ExpressJS",
-                image: "/images/expressjs.svg",
-            }
-        ],
-        database: [
-            {
-                id: 1,
-                name: "MongoDB",
-                image: "/images/mongodb.svg",
-            }
-        ],
-        library: [
-            {
-                id: 1,
-                name: "ReactJS",
-                image: "/images/reactjs.svg",
-            },
-            {
-                id: 2,
-                name: "NextJS",
-                image: "/images/nextjs.png",
-            },
-            {
-                id: 3,
-                name: "Chakra-UI",
-                image: "/images/chakraui.svg",
-            },
-            {
-                id: 4,
-                name: "Bootstrap",
-                image: "/images/bootstrap.svg",
-            },
-            {
-                id: 5,
-                name: "TailwindCSS",
-                image: "/images/tailwindcss.svg",
-            },
-            {
-                id: 6,
-                name: "Sass",
-                image: "/images/sass.svg",
-            },
-            {
-                id: 7,
-                name: "Styled Components",
-                image: "/images/styled-components.svg",
-            },
-        ],
-        tools: [
-            {
-                id: 1,
-                name: "Git",
-                image: "/images/git.svg",
-            },
-            {
-                id: 2,
-                name: "GitHub",
-                image: "/images/github.svg",
-            },
-            {
-                id: 3,
-                name: "GitLab",
-                image: "/images/gitlab.svg",
-            },
-            {
-                id: 4,
-                name: "Figma",
-                image: "/images/figma.svg",
-            },
-            {
-                id: 5,
-                name: "Framer Motion",
-                image: "/images/framer-motion.png",
-            },
-        ]
+        id: 1,
+        name: "HTML5",
+        image: "/images/html.svg",
+    },
+    {
+        id: 2,
+        name: "CSS3",
+        image: "/images/css.svg",
+    },
+    {
+        id: 3,
+        name: "JavaScript",
+        image: "/images/javascript.svg",
+    },
+];
+export const backend = [
+    {
+        id: 1,
+        name: "NodeJS",
+        image: "/images/nodejs.svg",
+    },
+    {
+        id: 2,
+        name: "ExpressJS",
+        image: "/images/expressjs.svg",
     }
-]
+];
+export const database = [
+    {
+        id: 1,
+        name: "MongoDB",
+        image: "/images/mongodb.svg",
+    }
+];
+export const library = [
+    {
+        id: 1,
+        name: "ReactJS",
+        image: "/images/reactjs.svg",
+    },
+    {
+        id: 2,
+        name: "NextJS",
+        image: "/images/nextjs.png",
+    },
+    {
+        id: 3,
+        name: "Chakra-UI",
+        image: "/images/chakraui.svg",
+    },
+    {
+        id: 4,
+        name: "Bootstrap",
+        image: "/images/bootstrap.svg",
+    },
+    {
+        id: 5,
+        name: "TailwindCSS",
+        image: "/images/tailwindcss.svg",
+    },
+    {
+        id: 6,
+        name: "Sass",
+        image: "/images/sass.svg",
+    },
+    {
+        id: 7,
+        name: "Styled Components",
+        image: "/images/styled-components.svg",
+    },
+];
+export const tools = [
+    {
+        id: 1,
+        name: "Git",
+        image: "/images/git.svg",
+    },
+    {
+        id: 2,
+        name: "GitHub",
+        image: "/images/github.svg",
+    },
+    {
+        id: 3,
+        name: "GitLab",
+        image: "/images/gitlab.svg",
+    },
+    {
+        id: 4,
+        name: "Figma",
+        image: "/images/figma.svg",
+    },
+    {
+        id: 5,
+        name: "Framer Motion",
+        image: "/images/framer-motion.png",
+    },
+];
