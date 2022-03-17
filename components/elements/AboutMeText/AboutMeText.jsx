4ce01f2eaf5cba0b21816e2e0c8f76d0803367ef
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-17 23:26:55 +0545

    feat & style(components): new components added and styled

--- components/elements/AboutMeText/AboutMeText.jsx
+++ components/elements/AboutMeText/AboutMeText.jsx
@@ -1,159 +1,161 @@
-import { Text, Link, Image } from "@chakra-ui/react";
+import {
+  Link,
+  Image,
+  Heading,
+  List,
+  ListItem,
+  ListIcon,
+  Box,
+} from "@chakra-ui/react";
 import { motion } from "framer-motion";
-import { commonVariants, linkStyle, specialLinkStyle } from "./AboutMeTextStyles";
+import {
+  commonVariants,
+  linkStyle,
+  specialLinkStyle,
+} from "./AboutMeTextStyles";
+import { FaCheckCircle, FaFreeCodeCamp } from "react-icons/fa";
+import { FiCornerDownRight } from "react-icons/fi";
+import { curriculums } from "../../../data/about-data";
 
-const MotionText = motion(Text)
+const MotionBox = motion(Box);
 
 const AboutMeText = () => {
-
-    return (
-        <>
-            <MotionText
-                p={{ base: "1.5rem 0rem", md: "2rem" }}
-                fontSize={{ base: "1.1rem", md: "1.2rem" }}
-                textAlign="center"
-                variants={commonVariants}
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true }}
-                transition={{
-                    delay: 0.5,
-                    duration: 0.5,
-                    type: "tween",
-                    stiffness: 110,
-                }}
-            >
-                A Front-End developing aficionado from{" "}
+  return (
+    <>
+      <MotionBox
+        p={{ base: "1.5rem 0rem", md: "1rem" }}
+        fontSize={{ base: "1.1rem", md: "1.2rem" }}
+        textAlign="center"
+        variants={commonVariants}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{
+          delay: 0.5,
+          duration: 0.5,
+          type: "tween",
+          stiffness: 110,
+        }}
+      >
+        A Front-End developing aficionado from{" "}
+        <Link
+          href="https://en.wikipedia.org/wiki/Nepal"
+          target="_blank"
+          rel="noreferrer noopener nofollow"
+          sx={linkStyle}
+          fontSize={{ base: "1.1rem", md: "1.5rem" }}
+        >
+          🇳🇵
+        </Link>{" "}
+        with an acute detail to hone the skills as required by the project as
+        well as modest experience of UI/UX designing proficiency and immense
+        interest to build elegant as well as fully functioning applications.
+        <br />
+        <br />
+        In the past, I have worked as a network technician and system engineer.
+        However, I always had secret affair with building interactive web
+        applications. As a result, during the pandemic, I had an epiphany to
+        learn web technologies seriously.
+        <br />
+        <br />
+        <Heading as="h3" fontSize="1.5rem">
+          Curriculums:
+        </Heading>
+        <Box m="1rem">
+          {curriculums.map((curriculum) => (
+            <List key={curriculum.id}>
+              <ListItem
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <ListIcon as={FaCheckCircle} />
                 <Link
-                    href="https://en.wikipedia.org/wiki/Nepal"
-                    target="_blank"
-                    rel="noreferrer noopener nofollow"
-                    sx={linkStyle}
-                    fontSize={{ base: "1.1rem", md: "1.5rem" }}
+                  fontSize="1.2rem"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                  sx={linkStyle}
+                  href={curriculum.link}
                 >
-                    🇳🇵
-                </Link>{" "}
-                with an acute detail to hone the skills as required by the
-                project as well as modest experience of UI/UX designing
-                proficiency and immense interest to build elegant as well as
-                fully functioning applications.
-                <br />
-                <br />
-                In the past, I have worked as a network technician and system
-                engineer. However, I always had secret affair with building
-                interactive web applications. During COVID19, I had an epiphany
-                to learn web development seriously. I took few courses from{" "}
-                <Link
-                    sx={specialLinkStyle}
-                    target="_blank"
-                    rel="noreferrer noopener nofollow"
-                    href="https://freecodecamp.org"
-                >
-                    freeCodeCamp
+                  {curriculum.title}
                 </Link>
-                , and followed many Youtube guru&apos;s like{" "}
                 <Link
-                    sx={specialLinkStyle}
-                    target="_blank"
-                    rel="noreferrer noopener nofollow"
-                    href="https://bit.ly/3sHIVhU"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                  sx={linkStyle}
+                  href="https://www.freecodecamp.org/"
                 >
-                    Kevin Powell
+                  <i>(freeCodeCamp)</i>
                 </Link>
-                ,{" "}
+              </ListItem>
+              <ListItem
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <FiCornerDownRight />
                 <Link
-                    sx={specialLinkStyle}
-                    target="_blank"
-                    rel="noreferrer noopener nofollow"
-                    href="https://bit.ly/3oJld3t"
+                  fontSize="1.2rem"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                  sx={linkStyle}
+                  href={curriculum.certificateUrl}
                 >
-                    Traversy Media
+                  {curriculum.title === "Front End Development Libraries"
+                    ? `Ongoing`
+                    : `Certificate` &&
+                      curriculum.title === "Back End Development and APIs"
+                    ? "Ongoing"
+                    : "Certificate"}
                 </Link>
-                ,{" "}
-                <Link
-                    sx={specialLinkStyle}
-                    target="_blank"
-                    rel="noreferrer noopener nofollow"
-                    href="https://bit.ly/3HPiehA"
-                >
-                    Web Dev Simplified
-                </Link>
-                ,{" "}
-                <Link
-                    sx={specialLinkStyle}
-                    target="_blank"
-                    rel="noreferrer noopener nofollow"
-                    href="https://bit.ly/3rHHHUs"
-                >
-                    The Net Ninja
-                </Link>
-                , and many more.
-                <br />
-                <br />
-                Without being ostentacious, I am a hard worker and a learner as
-                I have completed few certifications namely{" "}
-                <Link
-                    href="https://bit.ly/34z9OfC"
-                    sx={specialLinkStyle}
-                    rel="noreferrer noopener nofollow"
-                    target="_blank"
-                >
-                    Responsive Web Design Certification
-                </Link>{" "}
-                and{" "}
-                <Link
-                    href="https://bit.ly/3Bjjg2Y"
-                    sx={specialLinkStyle}
-                    rel="noreferrer noopener nofollow"
-                    target="_blank"
-                >
-                    JavaScript Algorithms and Data Structures Certification
-                </Link>{" "}
-                from freeCodeCamp. To further prove my hardworking habtitude,
-                during my university days, I along with my team was awarded with
-                the Best Software Project Award by{" "}
-                <Link
-                    href="https://nta.gov.np"
-                    sx={specialLinkStyle}
-                    rel="noreferrer noopener nofollow"
-                    target="_blank"
-                >
-                    Nepal Telecommunication Authority
-                </Link>
-                (NTA) for building Class Routine Management System.
-                <br />
-                <br />
-                Right now I am upskilling myself by learning ReactJS and NextJS
-                and have completed few projects using them which can be found in
-                my{" "}
-                <Link
-                    href="https://bit.ly/3LmRDue"
-                    sx={specialLinkStyle}
-                    rel="noopener noreferrer nofollow"
-                    target="_blank"
-                >
-                    GitHub
-                </Link>{" "}
-                profile and{" "}
-                <Link href="#projects" sx={specialLinkStyle}>
-                    Projects
-                </Link>{" "}
-                section.
-                <br />
-                <br />
-                As per my hobby, I am a football fanatic and secretly dream
-                about being a Rock &apos;n&apos; Roll guitarist{" "}
-                <Image
-                    src="/images/guitar.png"
-                    alt="Guitar"
-                    height="2rem"
-                    width="2rem"
-                    display="inline-block"
-                />
-                .
-            </MotionText>
-        </>
-    );
+              </ListItem>
+            </List>
+          ))}
+        </Box>
+        Without being ostentacious, to further prove my hardworking habtitude,
+        during my university days, I along with my team was awarded with the
+        Best Software Project Award by{" "}
+        <Link
+          href="https://nta.gov.np"
+          sx={specialLinkStyle}
+          rel="noreferrer noopener nofollow"
+          target="_blank"
+        >
+          Nepal Telecommunication Authority
+        </Link>
+        (NTA) for building Class Routine Management System.
+        <br />
+        <br />
+        Right now I am upskilling myself by learning ReactJS and NextJS and have
+        completed few projects using them which can be found in my{" "}
+        <Link
+          href="https://bit.ly/3LmRDue"
+          sx={specialLinkStyle}
+          rel="noopener noreferrer nofollow"
+          target="_blank"
+        >
+          GitHub
+        </Link>{" "}
+        profile and{" "}
+        <Link href="#projects" sx={specialLinkStyle}>
+          Projects
+        </Link>{" "}
+        section.
+        <br />
+        <br />
+        As per my hobby, I am a football fanatic and secretly dream about being
+        a Rock &apos;n&apos; Roll guitarist{" "}
+        <Image
+          src="/images/guitar.png"
+          alt="Guitar"
+          height="2rem"
+          width="2rem"
+          display="inline-block"
+        />
+        .
+      </MotionBox>
+    </>
+  );
 };
 
 export default AboutMeText;
--- components/elements/AboutMeText/AboutMeTextStyles.js
+++ components/elements/AboutMeText/AboutMeTextStyles.js
@@ -9,10 +9,24 @@ export const commonVariants = {
     }
 }
 
+export const wrapStyle = {
+    display: "flex",
+    fontSize: "1rem",
+    padding: {
+        base: "0.5rem 1.5rem",
+        md: "0.5rem 1.5rem",
+        lg: "1rem 3rem",
+    }
+}
+
 export const linkStyle = {
     _hover:{
         "&::after": { content: "none" },
     },
+    _focus: { outline: "none" },
+    _active: { outline: "none" },
+    textTransform: "none",
+    fontWeight: "normal"
 }
 
 export const specialLinkStyle = {
@@ -26,4 +40,5 @@ export const specialLinkStyle = {
     fontSize: { base: "1.1rem", md: "1.2rem" },
     fontWeight: "normal",
     textDecoration: "underline",
+    fontStyle: "italic",
 }
