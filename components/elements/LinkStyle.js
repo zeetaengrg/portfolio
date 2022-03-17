31e48729af627e1a3845c24bdc7a634e92c232e2
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-17 23:34:54 +0545

    feat(components, data and images): new components, data created and new images added

---
+++ components/elements/LinkStyle.js
@@ -0,0 +1,10 @@
+export const linkStyle = {
+    _hover: {
+        "&::after": { content: "none" },
+        color: "#a5abbd",
+        textDecoration: "none",
+    },
+    _focus: { boxShadow: "none" },
+    target: "_blank",
+    rel: "noopener noreferrer nofollow",
+}
