bfe29c6cb6697f354143aeffabe6b060cb3a4fa6
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-21 22:13:54 +0545

    refactor(data)

--- data/projects-data.js
+++ data/projects-data.js
@@ -1,105 +1,104 @@
 export const projects = [
-    {
+  {
+    id: 1,
+    titleOne: "Hulu Landing Page",
+    titleTwo: "Portfolio Website",
+    descriptionOne:
+      "A fully responsive design clone of Hulu's Landing Page with a little bit of Javascript for 'Login' modal functionality. Built it from scratch with pixel perfect design of Hulu's Website to put the skills to test. Learned Media Queries and JavaScript DOM manipulation techniques.",
+    descriptionTwo:
+      "A personal portfolio website which showcases the skills, tools and technologies used and recent projects. Having learned how to build Portfolio website from Kevin Powell using HTML, CSS and JavaScript, to put the skills to test and also to build personal Portfolio Website, it was built from scratch to implement NextJS framework.",
+    imageOne: "/images/hulu.webp",
+    demoLinkOne: "https://hulu-landing-page-clone.vercel.app/",
+    sourceLinkOne: "https://github.com/zeetaen1989/hulu-landing-page-clone",
+    imageTwo: "/images/portfolio.webp",
+    demoLinkTwo: "https://www.jitendragurung.com.np/",
+    sourceLinkTwo: "https://github.com/zeetaen1989/portfolio",
+    technologyOne: [
+      {
         id: 1,
-        titleOne: "Hulu Landing Page",
-        titleTwo: "Portfolio Website",
-        descriptionOne:
-            "A fully responsive design clone of Hulu's Landing Page with a little bit of Javascript for 'Login' modal functionality. Built it from scratch with pixel perfect design of Hulu's Website to put the skills to test. Learned Media Queries and JavaScript DOM manipulation techniques.",
-        descriptionTwo:
-            "A personal portfolio website which showcases the skills, tools and technologies used and recent projects. Having learned how to build Portfolio website from Kevin Powell using HTML, CSS and JavaScript, to put the skills to test and also to build personal Portfolio Website, it was built from scratch to implement NextJS framework.",
-        imageOne: "/images/hulu.webp",
-        demoLinkOne: "https://hulu-landing-page-clone.vercel.app/",
-        sourceLinkOne: "https://github.com/zeetaen1989/hulu-landing-page-clone",
-        imageTwo: "/images/portfolio.webp",
-        demoLinkTwo: "https://www.jitendragurung.com.np/",
-        sourceLinkTwo: "https://github.com/zeetaen1989/portfolio",
-        technologyOne: [
-            {
-                id: 1,
-                name: "HTML",
-                image: "/images/html.svg",
-            },
-            {
-                id: 2,
-                name: "CSS",
-                image: "/images/css.svg",
-            },
-            {
-                id: 3,
-                name: "JavaScript",
-                image: "/images/javascript.svg",
-            }
-        ],
-        technologyTwo: [
-            {
-                id: 1,
-                name: "NextJS",
-                image: "/images/nextjs.png",
-            },
-            {
-                id: 2,
-                name: "Chakra-UI",
-                image: "/images/chakraui.svg",
-            },
-            {
-                id: 3,
-                name: "Framer Motion",
-                image: "/images/framer-motion.png",
-            }
-        ]
-    },
-    {
+        name: "HTML",
+        image: "/images/html.svg",
+      },
+      {
         id: 2,
-        titleOne: "Mama's Joint",
-        titleTwo: "E-Commerce Website",
-        descriptionOne:
-            `This is a fully responsive restaurant's landing page where you can browse the popular menu and get information about the restaurant in detail. Built from scratch with pixel perfect design provided by AR Shakir.`,
-        descriptionTwo:
-            "This is a clothing store e-commerce website built to demonstrate full stack development technologies. This project is under construction.",
-        imageOne: "/images/mamas-joint.png",
-        demoLinkOne: "https://mamas-joint.vercel.app/",
-        sourceLinkOne: "https://github.com/zeetaen1989/mamas-joint",
-        imageTwo: "/images/clothingstore.webp",
-        demoLinkTwo: "#",
-        sourceLinkTwo: "https://github.com/zeetaen1989/e-commerce-clothing-shop",
-        technologyOne: [
-            {
-                id: 1,
-                name: "NextJS",
-                image: "/images/nextjs.png",
-            },
-            {
-                id: 2,
-                name: "Sass",
-                image: "/images/sass.svg",
-            },
-        ],
-        technologyTwo: [
-            {
-                id: 1,
-                name: "ReactJS",
-                image: "/images/reactjs.svg",
-            },
-            {
-                id: 2,
-                name: "Styled-Components",
-                image: "/images/styled-components.svg",
-            },
-            {
-                id: 3,
-                name: "NodeJS",
-                image: "/images/nodejs.svg",
-            },
-            {
-                id: 4,
-                name: "ExpressJS",
-                image: "/images/expressjs.svg",
-            },
-            {
-                id: 5,
-                name: "MongoDB",
-                image: "/images/mongodb.svg",
-            },
-        ]
-    },
+        name: "CSS",
+        image: "/images/css.svg",
+      },
+      {
+        id: 3,
+        name: "JavaScript",
+        image: "/images/javascript.svg",
+      },
+    ],
+    technologyTwo: [
+      {
+        id: 1,
+        name: "NextJS",
+        image: "/images/nextjs.png",
+      },
+      {
+        id: 2,
+        name: "Chakra-UI",
+        image: "/images/chakraui.svg",
+      },
+      {
+        id: 3,
+        name: "Framer Motion",
+        image: "/images/framer-motion.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    titleOne: "Mama's Joint",
+    titleTwo: "E-Commerce Website",
+    descriptionOne: `This is a fully responsive restaurant's landing page where you can browse the popular menu and get information about the restaurant in detail. Built from scratch with pixel perfect design provided by AR Shakir.`,
+    descriptionTwo:
+      "This is a clothing store e-commerce website built to demonstrate full stack development technologies. This project is under construction.",
+    imageOne: "/images/mamas-joint.png",
+    demoLinkOne: "https://mamas-joint.vercel.app/",
+    sourceLinkOne: "https://github.com/zeetaen1989/mamas-joint",
+    imageTwo: "/images/clothingstore.webp",
+    demoLinkTwo: "#",
+    sourceLinkTwo: "https://github.com/zeetaen1989/e-commerce-clothing-shop",
+    technologyOne: [
+      {
+        id: 1,
+        name: "NextJS",
+        image: "/images/nextjs.png",
+      },
+      {
+        id: 2,
+        name: "Sass",
+        image: "/images/sass.svg",
+      },
+    ],
+    technologyTwo: [
+      {
+        id: 1,
+        name: "ReactJS",
+        image: "/images/reactjs.svg",
+      },
+      {
+        id: 2,
+        name: "Styled-Components",
+        image: "/images/styled-components.svg",
+      },
+      {
+        id: 3,
+        name: "NodeJS",
+        image: "/images/nodejs.svg",
+      },
+      {
+        id: 4,
+        name: "ExpressJS",
+        image: "/images/expressjs.svg",
+      },
+      {
+        id: 5,
+        name: "MongoDB",
+        image: "/images/mongodb.svg",
+      },
+    ],
+  },
 ];
