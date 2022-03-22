05dfcf2ceb3eb409632ee6eaa8bda08c8f915806
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-22 17:23:36 +0545

    style(blob images): smaller screens blob image sized modified

--- components/templates/Hero/Hero.jsx
+++ components/templates/Hero/Hero.jsx
@@ -3,18 +3,18 @@ import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { DownloadBtn, HireMeBtn } from "../../elements";
 import {
-    blobOneVariants,
-    blobTwoVariants,
-    blobThreeVariants,
-    blobFourVariants,
-    commonVariants,
-    blobOneStyle,
-    blobTwoStyle,
-    blobThreeStyle,
-    blobFourStyle,
-    flexStyle,
-    imgStyle,
-    linkStyle,
+  blobOneVariants,
+  blobTwoVariants,
+  blobThreeVariants,
+  blobFourVariants,
+  commonVariants,
+  blobOneStyle,
+  blobTwoStyle,
+  blobThreeStyle,
+  blobFourStyle,
+  flexStyle,
+  imgStyle,
+  linkStyle,
 } from "./HeroStyle";
 
 const MotionImage = motion(Image);
@@ -26,155 +26,147 @@ const MotionText = motion(Text);
 const MotionIcon = motion(Box);
 
 const Hero = () => {
-
-    return (
-        <Box mt={{ base: "5rem", md: "0rem" }}>
-            <Box>
-                <BlobMotionImage
-                    src="/images/blob1.svg"
-                    alt="blob"
-                    sx={blobOneStyle}
-                    variants={blobOneVariants}
-                    initial="start"
-                    animate="finish"
-                />
-                <BlobMotionImage
-                    src="/images/blob2.svg"
-                    alt="blob"
-                    sx={blobTwoStyle}
-                    variants={blobTwoVariants}
-                    initial="start"
-                    animate="finish"
-                />
-                <BlobMotionImage
-                    src="/images/blob3.svg"
-                    alt="blob"
-                    sx={blobThreeStyle}
-                    variants={blobThreeVariants}
-                    initial="start"
-                    animate="finish"
-                />
-                <BlobMotionImage
-                    src="/images/blob4.png"
-                    alt="blob"
-                    sx={blobFourStyle}
-                    variants={blobFourVariants}
-                    initial="start"
-                    animate="finish"
-                />
-            </Box>
-            <Flex sx={flexStyle}>
-                <Box>
-                    <MotionImage
-                        sx={imgStyle}
-                        src="/images/jiten.webp"
-                        alt="Jiten Image"
-                        variants={commonVariants}
-                        initial="hidden"
-                        animate="visible"
-                    />
-                </Box>
-                <Box>
-                    <Flex
-                        direction="column"
-                        textAlign={{ base: "center", md: "start" }}
-                    >
-                        <MotionText
-                            casing="uppercase"
-                            fontSize={{ base: "1rem", md: "1.5rem" }}
-                            variants={commonVariants}
-                            initial="hidden"
-                            animate="visible"
-                        >
-                            Hi!
-                        </MotionText>
-                        <Box fontSize={{ base: "1.5rem", md: "2rem" }}>
-                            <Flex
-                                justifyContent={{ base: "center", md: "start" }}
-                            >
-                                <MotionText
-                                    fontWeight="bold"
-                                    casing="uppercase"
-                                    variants={commonVariants}
-                                    initial="hidden"
-                                    animate="visible"
-                                >
-                                    I&apos;m
-                                </MotionText>
-                                <MotionIcon
-                                    variants={commonVariants}
-                                    initial="hidden"
-                                    animate="visible"
-                                >
-                                    <FaQuoteLeft
-                                        style={{
-                                            marginLeft: "0.5rem",
-                                            fontSize: "0.8rem",
-                                        }}
-                                    />
-                                </MotionIcon>
-                                <MotionText
-                                    casing="uppercase"
-                                    color="#fff"
-                                    fontSize={{ base: "3rem", md: "4rem" }}
-                                    fontWeight="extrabold"
-                                    lineHeight="1.4"
-                                    variants={commonVariants}
-                                    initial="hidden"
-                                    animate="visible"
-                                >
-                                    Jiten
-                                </MotionText>
-                                <MotionIcon
-                                    variants={commonVariants}
-                                    initial="hidden"
-                                    animate="visible"
-                                >
-                                    <FaQuoteRight
-                                        style={{
-                                            marginLeft: "0.5rem",
-                                            fontSize: "0.8rem",
-                                        }}
-                                    />
-                                </MotionIcon>
-                            </Flex>
-                        </Box>
-                        <Box>
-                            <MotionText
-                                casing="uppercase"
-                                fontSize={{ base: "2.5rem", md: "3rem" }}
-                                fontWeight="bold"
-                                lineHeight="1"
-                                variants={commonVariants}
-                                initial="hidden"
-                                animate="visible"
-                            >
-                                Front End Dev
-                            </MotionText>
-                        </Box>
-                        <Box>
-                            <Flex
-                                gap="2rem"
-                                mt="2rem"
-                                justifyContent={{ base: "center", md: "left" }}
-                            >
-                                <Link
-                                    href="/cv/Resume.pdf"
-                                    download
-                                    sx={linkStyle}
-                                >
-                                    <DownloadBtn />
-                                </Link>
-                                <Link href="#contact" sx={linkStyle}>
-                                    <HireMeBtn />
-                                </Link>
-                            </Flex>
-                        </Box>
-                    </Flex>
-                </Box>
-            </Flex>
+  return (
+    <Box mt={{ base: "5rem", md: "0rem" }}>
+      <Box>
+        <BlobMotionImage
+          src="/images/blob1.svg"
+          alt="blob"
+          sx={blobOneStyle}
+          variants={blobOneVariants}
+          initial="start"
+          animate="finish"
+        />
+        <BlobMotionImage
+          src="/images/blob2.svg"
+          alt="blob"
+          sx={blobTwoStyle}
+          variants={blobTwoVariants}
+          initial="start"
+          animate="finish"
+        />
+        <BlobMotionImage
+          src="/images/blob3.svg"
+          alt="blob"
+          sx={blobThreeStyle}
+          variants={blobThreeVariants}
+          initial="start"
+          animate="finish"
+        />
+        <BlobMotionImage
+          src="/images/blob4.png"
+          alt="blob"
+          sx={blobFourStyle}
+          variants={blobFourVariants}
+          initial="start"
+          animate="finish"
+        />
+      </Box>
+      <Flex sx={flexStyle}>
+        <Box>
+          <MotionImage
+            sx={imgStyle}
+            src="/images/jiten.webp"
+            alt="Jiten Image"
+            variants={commonVariants}
+            initial="hidden"
+            animate="visible"
+            width={{ base: "8rem", md: "12rem", lg: "17rem" }}
+            height="100%"
+          />
         </Box>
-    );
+        <Box>
+          <Flex direction="column" textAlign={{ base: "center", md: "start" }}>
+            <MotionText
+              casing="uppercase"
+              fontSize={{ base: "1rem", md: "1.5rem" }}
+              variants={commonVariants}
+              initial="hidden"
+              animate="visible"
+            >
+              Hi!
+            </MotionText>
+            <Box fontSize={{ base: "1.5rem", md: "2rem" }}>
+              <Flex justifyContent={{ base: "center", md: "start" }}>
+                <MotionText
+                  fontWeight="bold"
+                  casing="uppercase"
+                  variants={commonVariants}
+                  initial="hidden"
+                  animate="visible"
+                >
+                  I&apos;m
+                </MotionText>
+                <MotionIcon
+                  variants={commonVariants}
+                  initial="hidden"
+                  animate="visible"
+                >
+                  <FaQuoteLeft
+                    style={{
+                      marginLeft: "0.5rem",
+                      fontSize: "0.8rem",
+                    }}
+                  />
+                </MotionIcon>
+                <MotionText
+                  casing="uppercase"
+                  color="#fff"
+                  fontSize={{ base: "3rem", md: "4rem" }}
+                  fontWeight="extrabold"
+                  lineHeight="1.4"
+                  variants={commonVariants}
+                  initial="hidden"
+                  animate="visible"
+                >
+                  Jiten
+                </MotionText>
+                <MotionIcon
+                  variants={commonVariants}
+                  initial="hidden"
+                  animate="visible"
+                >
+                  <FaQuoteRight
+                    style={{
+                      marginLeft: "0.5rem",
+                      fontSize: "0.8rem",
+                    }}
+                  />
+                </MotionIcon>
+              </Flex>
+            </Box>
+            <Box>
+              <MotionText
+                casing="uppercase"
+                fontSize={{ base: "2.5rem", md: "3rem" }}
+                fontWeight="bold"
+                lineHeight="1"
+                variants={commonVariants}
+                initial="hidden"
+                animate="visible"
+              >
+                Front End Dev
+              </MotionText>
+            </Box>
+            <Box>
+              <Flex
+                gap="2rem"
+                mt="2rem"
+                justifyContent={{ base: "center", md: "left" }}
+              >
+                <Link href="/cv/Resume.pdf" download sx={linkStyle}>
+                  <DownloadBtn />
+                </Link>
+                <Link href="#contact" sx={linkStyle}>
+                  <HireMeBtn />
+                </Link>
+              </Flex>
+            </Box>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Hero;
