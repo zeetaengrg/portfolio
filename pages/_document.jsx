290914be77db7faffcc1d08f8a1a98475f90e34e
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-24 16:46:37 +0545

    fix(meta tag): meta tag with charset attribute issue fixed

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,16 +1,26 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang="en">
         <Head>
-          <meta charSet='utf-8' />
-          <meta name='keywords' content='Portfolio, Portfolio Website, Jiten, zeetaen, Jiten Gurung, Jitendra, Jitendra Kumar Gurung' />
-          <meta name='description' content='Portfolio Website of Jiten Gurung' />
-          <meta name='author' content='Jitendra Gurung, Jiten Gurung' />
-          <meta name='copyright' content='Jiten Gurung' />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"></link>
+          <meta charset="utf-8" />
+          <meta name="keywords" content="Jitendra, Jitendra Gurung, zeetaen" />
+          <meta
+            name="description"
+            content="Portfolio Website of Jiten Gurung"
+          />
+          <meta name="author" content="Jitendra Gurung" />
+          <meta name="copyright" content="Jitendra Gurung" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          ></link>
+          <link
+            rel="shortcut icon"
+            href="/favicon.ico"
+            type="image/x-icon"
+          ></link>
           <link rel="icon" href="/favicon.ico" type="image/x-icon"></link>
         </Head>
         <body>
@@ -18,6 +28,6 @@ export default class Document extends NextDocument {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
