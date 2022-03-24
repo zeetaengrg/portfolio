31fcc47fda77c0a40d0c7e7a682bb36901e385eb
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-24 20:28:27 +0545

    fix(prettier): code prettified

--- components/layouts/ThankYou/ThankYou.jsx
+++ components/layouts/ThankYou/ThankYou.jsx
@@ -1,27 +1,25 @@
 import { Box, Divider, Flex, Heading, Text } from "@chakra-ui/react";
 import { FaPrayingHands } from "react-icons/fa";
-import { GoBackBtn } from "../../elements";
+import { GoBackBtn } from "@components/elements";
 import { flexStyle, headingStyle, textStyle, boxStyle } from "./ThankYouStyles";
 
 const ThankYou = () => {
-
-    return (
-        <>
-            <Flex sx={flexStyle}>
-                <Box sx={boxStyle}>
-                    <Heading sx={headingStyle}>
-                        Thank You!!!
-                    </Heading>
-                        <Divider width="60%" />
-                    <Text sx={textStyle}>
-                        Your message has been sent successfully. I will get back to you shortly.
-                    </Text>
-                    <FaPrayingHands style={{ fontSize: "2rem" }} />
-                </Box>
-                <GoBackBtn />
-            </Flex>
-        </>
-    );
+  return (
+    <>
+      <Flex sx={flexStyle}>
+        <Box sx={boxStyle}>
+          <Heading sx={headingStyle}>Thank You!!!</Heading>
+          <Divider width="60%" />
+          <Text sx={textStyle}>
+            Your message has been sent successfully. I will get back to you
+            shortly.
+          </Text>
+          <FaPrayingHands style={{ fontSize: "2rem" }} />
+        </Box>
+        <GoBackBtn />
+      </Flex>
+    </>
+  );
 };
 
 export default ThankYou;
