3de79753ac666bd50831eb4f5aa1471bfc652c0e
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-24 20:27:37 +0545

    feat(path aliases): absolute imports and module path aliases configured

--- components/layouts/Error/Error.jsx
+++ components/layouts/Error/Error.jsx
@@ -1,47 +1,40 @@
-import { Box, Flex, Image, Text } from '@chakra-ui/react';
-import { GoBackBtn } from '../../elements';
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { GoBackBtn } from "@components/elements";
 
 const Error = () => {
+  const textStyle = {
+    textAlign: "center",
+    color: "#fff",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+  };
 
-    const textStyle = {
-        textAlign: "center",
-        color: "#fff",
-        fontWeight: 'bold',
-        textTransform: "uppercase",
-    }
-
-    return (
-        <>
-            <Box m={{ base: "1.5rem", md: "2.5rem" }} >
-                <Flex
-                    direction="column"
-                    justifyContent="center"
-                    alignItems="center"
-                    gap={{ base: "0rem", md: "1rem" }}
-                >
-                    <Text
-                        fontSize={{ base: "1.1rem", md: "1.3rem" }}
-                        sx={textStyle}
-                    >
-                        Oops!!! Page Not Found
-                    </Text>
-                    <Image
-                        src="/images/error.svg"
-                        alt="404 Error"
-                        width={{ base: "80%", md: "50%" }}
-                        m="2.5rem 0"
-                    />
-                    <Text
-                        sx={textStyle}
-                        fontSize={{ base: "1.2rem", md: "1.3rem" }}
-                    >
-                        Sorry, but the page you requested was not found!
-                    </Text>
-                    <GoBackBtn />
-                </Flex>
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <Box m={{ base: "1.5rem", md: "2.5rem" }}>
+        <Flex
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          gap={{ base: "0rem", md: "1rem" }}
+        >
+          <Text fontSize={{ base: "1.1rem", md: "1.3rem" }} sx={textStyle}>
+            Oops!!! Page Not Found
+          </Text>
+          <Image
+            src="/images/error.svg"
+            alt="404 Error"
+            width={{ base: "80%", md: "50%" }}
+            m="2.5rem 0"
+          />
+          <Text sx={textStyle} fontSize={{ base: "1.2rem", md: "1.3rem" }}>
+            Sorry, but the page you requested was not found!
+          </Text>
+          <GoBackBtn />
+        </Flex>
+      </Box>
+    </>
+  );
 };
 
 export default Error;
