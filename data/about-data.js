d01692e2c49127f6788ebf76b72b4a53a118795a
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-25 20:32:09 +0545

    feat(data): new data added to about data

--- data/about-data.js
+++ data/about-data.js
@@ -4,22 +4,25 @@ export const curriculums = [
     title: "Responsive Web Design",
     platform: "freeCodeCamp",
     link: "https://www.freecodecamp.org/learn/responsive-web-design/",
-    certificateUrl: "https://www.freecodecamp.org/certification/zeetaen/responsive-web-design"
+    certificateUrl:
+      "https://www.freecodecamp.org/certification/zeetaen/responsive-web-design",
   },
   {
     id: 2,
     title: "JavaScript Data Structure and Algorithm",
     platform: "freeCodeCamp",
     link: "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/",
-    certificateUrl: "https://www.freecodecamp.org/certification/zeetaen/javascript-algorithms-and-data-structures",
+    certificateUrl:
+      "https://www.freecodecamp.org/certification/zeetaen/javascript-algorithms-and-data-structures",
   },
-  // {
-  //   id: 3,
-  //   title: "Front End Development Libraries",
-  //   platform: "freeCodeCamp",
-  //   link: "https://www.freecodecamp.org/learn/front-end-development-libraries/",
-  //   certificateUrl: "#"
-  // },
+  {
+    id: 3,
+    title: "Front End Development Libraries",
+    platform: "freeCodeCamp",
+    link: "https://www.freecodecamp.org/learn/front-end-development-libraries/",
+    certificateUrl:
+      "https://www.freecodecamp.org/certification/zeetaen/front-end-development-libraries",
+  },
   // {
   //   id: 4,
   //   title: "Back End Development and APIs",
@@ -27,4 +30,4 @@ export const curriculums = [
   //   link: "https://www.freecodecamp.org/learn/back-end-development-and-apis/",
   //   certificateUrl: "#"
   // }
-]
+];
