d1f8875cf861f0f4616cdcfdecf789ab2c87237c
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-28 11:48:41 +0545

    style(hover): transition while hover modified

--- components/elements/Btn/Btn.js
+++ components/elements/Btn/Btn.js
@@ -2,9 +2,12 @@ export const btnStyle = {
   padding: "0 0.8rem",
   backgroundColor: "#141E39",
   fontSize: "1.2rem",
-  transition: "all 0.3s ease-in-out",
   _focus: { boxShadow: "none" },
-  _hover: { bg: "#a5abbd", color: "#0b111e" },
+  _hover: {
+    bg: "#a5abbd",
+    color: "#0b111e",
+    transition: "all 0.3s ease-in-out",
+  },
   _active: { bg: "#a5abbd", color: "#0b111e" },
   boxShadow:
     "4px 4px 8px rgba(0, 0, 0, 0.27), 0 0 40px rgba(0, 0, 0, 0.06) inset",
