652eed2e1d397590bcd6c5f8a73a2331d7b91130
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-29 19:08:33 +0545

    feat(props): width props passed to the child components

--- components/elements/LineDivider/LineDivider.jsx
+++ components/elements/LineDivider/LineDivider.jsx
@@ -1,33 +1,33 @@
 import { Center, Divider } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
-const MotionDivider = motion(Divider)
+const MotionDivider = motion(Divider);
 
 const commonVariants = {
-    hidden: {
-        opacity: 0,
-        y: "5vh",
-    },
-    visible: {
-        opacity: 1,
-        y: 0,
-    }
-}
+  hidden: {
+    opacity: 0,
+    y: "5vh",
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
-const LineDivider = () => {
-    return (
-        <Center>
-            <MotionDivider 
-                width={{ base: "40%", md: "18%" }}
-                mb="0.5rem"
-                variants={commonVariants}
-                initial="hidden"
-                whileInView="visible"
-                transition={{ delay: 0.2, duration: 0.5, type: "tween" }}
-                viewport={{ once: true }} 
-            />
-        </Center>
-    )
-}
+const LineDivider = ({ width }) => {
+  return (
+    <Center>
+      <MotionDivider
+        width={width}
+        mb="0.5rem"
+        variants={commonVariants}
+        initial="hidden"
+        whileInView="visible"
+        transition={{ delay: 0.2, duration: 0.5, type: "tween" }}
+        viewport={{ once: true }}
+      />
+    </Center>
+  );
+};
 
-export default LineDivider
+export default LineDivider;
