6f82164d586e8b0296bec1456b78675102a6b001
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-30 14:14:23 +0545

    refactor(bundle size)

--- components/elements/ScrollBackToTopBtn/ScrollBackToTopBtn.jsx
+++ components/elements/ScrollBackToTopBtn/ScrollBackToTopBtn.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Box, Button } from "@chakra-ui/react";
 import { FaArrowAltCircleUp } from "react-icons/fa";
 import { btn } from "./ScrollBackToTopBtnStyles";
