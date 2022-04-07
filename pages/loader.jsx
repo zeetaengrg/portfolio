6ac769efc0dd2e86df65723966b7f22f053f938f
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-07 10:33:51 +0545

    feat(pages): loader page created

---
+++ pages/loader.jsx
@@ -0,0 +1,15 @@
+import { Loader } from "@components/elements";
+import Head from "next/head";
+
+const LoaderPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Jiten Gurung | Portfolio Website</title>
+      </Head>
+      <Loader />
+    </>
+  );
+};
+
+export default LoaderPage;
