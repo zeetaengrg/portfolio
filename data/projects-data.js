2135c096266e83a30c4eef4b26e9ec11cc186bbc
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-11 14:10:36 +0545

    feat: local data modified

--- data/projects-data.js
+++ data/projects-data.js
@@ -1,71 +1,18 @@
 export const projects = [
   {
     id: 1,
-    titleOne: "Hulu Landing Page",
-    titleTwo: "Portfolio Website",
-    descriptionOne:
-      "A fully responsive design clone of Hulu's Landing Page with a little bit of Javascript for 'Login' modal functionality. Built it from scratch with pixel perfect design of Hulu's Website to put the skills to test. Learned Media Queries and JavaScript DOM manipulation techniques.",
-    descriptionTwo:
-      "A personal portfolio website which showcases the skills, tools and technologies used and recent projects. Having learned how to build Portfolio website from Kevin Powell using HTML, CSS and JavaScript, to put the skills to test and also to build personal Portfolio Website, it was built from scratch to implement NextJS framework.",
-    imageOne: "/images/hulu.webp",
-    demoLinkOne: "https://hulu-landing-page-clone.vercel.app/",
-    sourceLinkOne: "https://github.com/zeetaen1989/hulu-landing-page-clone",
-    imageTwo: "/images/portfolio.webp",
-    demoLinkTwo: "https://www.jitendragurung.com.np/",
-    sourceLinkTwo: "https://github.com/zeetaen1989/portfolio",
-    technologyOne: [
-      {
-        id: 1,
-        name: "HTML",
-        image: "/images/html.svg",
-      },
-      {
-        id: 2,
-        name: "CSS",
-        image: "/images/css.svg",
-      },
-      {
-        id: 3,
-        name: "JavaScript",
-        image: "/images/javascript.svg",
-      },
-    ],
-    technologyTwo: [
-      {
-        id: 1,
-        name: "NextJS",
-        image: "/images/nextjs.svg",
-      },
-      {
-        id: 2,
-        name: "Chakra-UI",
-        image: "/images/chakraui.svg",
-      },
-      {
-        id: 3,
-        name: "Framer Motion",
-        image: "/images/framer-motion.png",
-      },
-      {
-        id: 4,
-        name: "Figma",
-        image: "/images/figma.svg",
-      },
-    ],
-  },
-  {
-    id: 2,
     titleOne: "Mama's Joint",
-    titleTwo: "E-Commerce Website",
-    descriptionOne: `This is a fully responsive restaurant's landing page where you can browse the popular menu and get information about the restaurant in detail. Built from scratch with pixel perfect design provided by AR Shakir.`,
+    titleTwo: "Pizza Land",
+    descriptionOne:
+      "This is a fully responsive restaurant's landing page. Built from scratch with pixel perfect design provided by AR Shakir. This is a personal project to showcase SASS design skills using SASS modules.",
     descriptionTwo:
-      "This is a clothing store e-commerce website built to demonstrate full stack development technologies. This project is under construction.",
+      "A digital platform to order pizza with delivery service. This is a personal project to learn and showcase NextJS skills along with Redux Toolkit. Server Side Rendering technique is used to fetch API from server. Furthermore, new features like user authentication and admin panel for CRUD operation is under development.",
     imageOne: "/images/mamas-joint.webp",
     demoLinkOne: "https://mamas-joint.vercel.app/",
     sourceLinkOne: "https://github.com/zeetaen1989/mamas-joint",
-    imageTwo: "/images/alpha-male.webp",
+    imageTwo: "/images/pizza-land.png",
     demoLinkTwo: "#",
-    sourceLinkTwo: "https://github.com/zeetaen1989/e-commerce-clothing-shop",
+    sourceLinkTwo: "https://github.com/zeetaen1989/pizza-delivery",
     technologyOne: [
       {
         id: 1,
@@ -84,6 +31,48 @@ export const projects = [
       },
     ],
     technologyTwo: [
+      {
+        id: 1,
+        name: "NextJS",
+        image: "/images/nextjs.svg",
+      },
+      {
+        id: 2,
+        name: "Sass",
+        image: "/images/sass.svg",
+      },
+      {
+        id: 3,
+        name: "Redux Toolkit",
+        image: "/images/redux.svg",
+      },
+      {
+        id: 4,
+        name: "MongoDB",
+        image: "/images/mongodb.svg",
+      },
+      {
+        id: 5,
+        name: "Postman",
+        image: "/images/postman.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    titleOne: "E-Commerce Website",
+    titleTwo: "Portfolio Website",
+    descriptionOne:
+      "A clothing store e-commerce website built using MERN full stack development. Features like user authentication using JWT and admin panel dashboard for CRUD operations has been added and is under final stages of deployment.",
+    descriptionTwo:
+      "My personal portfolio website to showcase my skills and projects I have been working on to upskill my knowledge and expertise.",
+    imageOne: "/images/alpha-male.webp",
+    demoLinkOne: "#",
+    sourceLinkOne: "https://github.com/zeetaen1989/e-commerce-clothing-shop",
+    imageTwo: "/images/portfolio.webp",
+    demoLinkTwo: "https://jitendragurung.com.np/",
+    sourceLinkTwo: "https://github.com/zeetaen1989/portfolio",
+    technologyOne: [
       {
         id: 1,
         name: "ReactJS",
@@ -108,6 +97,33 @@ export const projects = [
         id: 5,
         name: "MongoDB",
         image: "/images/mongodb.svg",
+      },
+      {
+        id: 6,
+        name: "Postman",
+        image: "/images/postman.png",
+      },
+    ],
+    technologyTwo: [
+      {
+        id: 1,
+        name: "NextJS",
+        image: "/images/nextjs.svg",
+      },
+      {
+        id: 2,
+        name: "Chakra UI",
+        image: "/images/chakraui.svg",
+      },
+      {
+        id: 3,
+        name: "Framer Motion",
+        image: "/images/framer-motion.png",
+      },
+      {
+        id: 4,
+        name: "Figma",
+        image: "/images/figma.svg",
       },
     ],
   },
