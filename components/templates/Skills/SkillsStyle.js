d3a57f97bccde9ab2306ac5886bd9521b584a33c
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-05 18:28:21 +0545

    refactor: indentations modified

--- components/templates/Skills/SkillsStyle.js
+++ components/templates/Skills/SkillsStyle.js
@@ -1,17 +1,15 @@
-export const tabList = [
-    "Language", "UI Design", "Tools"
-];
+export const tabList = ["Language", "UI Design", "Tools"];
 
 export const tabStyle = {
-    margin: { base: "0.5rem", md: "1.3rem" },
-    fontSize: { base: "1rem", md: "1.2rem" },
-    color: "#a5abbd",
-    _hover: { color: "white" },
-    _focus: { outline: "none" },
-    _active: { bg: "none" },
-    _selected: { bg: "#141E39", border: "1px solid #a5abbd", color: "white" },
+  margin: { base: "0.5rem", md: "1.3rem " },
+  fontSize: { base: "1rem", md: "1.2rem" },
+  color: "#a5abbd",
+  _hover: { color: "white" },
+  _focus: { outline: "none" },
+  _active: { bg: "none" },
+  _selected: { bg: "#141E39", border: "1px solid #a5abbd", color: "white" },
 };
 
 export const iconStyle = {
-    marginLeft: "0.2rem",
-}
+  marginLeft: "0.2rem",
+};
