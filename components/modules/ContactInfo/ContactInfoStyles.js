367f2e41e79761d0905630ec70dbaaf695e4f2f2
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-08 21:46:58 +0545

    feat: profile links updated

--- components/modules/ContactInfo/ContactInfoStyles.js
+++ components/modules/ContactInfo/ContactInfoStyles.js
@@ -1,54 +1,54 @@
 export const commonVariants = {
-    hidden: {
-        opacity: 0,
-        y: "5vh",
-    },
-    visible: {
-        opacity: 1,
-        y: 0,
-    }
-}
+  hidden: {
+    opacity: 0,
+    y: "5vh",
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 export const links = [
-    {
-        name: "LinkedIn Icon",
-        url: "https://www.linkedin.com/in/zeetaen1878/",
-    },
-    {
-        name: "Github Icon",
-        url: "https://github.com/zeetaen1989"
-    },
-    {
-        name: "Codepen Icon",
-        url: "https://codepen.io/zeetaen1989"
-    }
-]
+  {
+    name: "LinkedIn Icon",
+    url: "https://www.linkedin.com/in/zeetaengrg/",
+  },
+  {
+    name: "Github Icon",
+    url: "https://github.com/zeetaengrg",
+  },
+  {
+    name: "Codepen Icon",
+    url: "https://codepen.io/zeetaen",
+  },
+];
 
 export const iconStyle = {
-    fontSize: "2rem",
+  fontSize: "2rem",
 };
 
 export const textStyle = {
-    fontSize: "1.2rem",
-    textTransform: "none",
+  fontSize: "1.2rem",
+  textTransform: "none",
 };
 
 export const flexStyle = {
-    gap: "1rem",
-    alignItems: "center",
+  gap: "1rem",
+  alignItems: "center",
 };
 
 export const socialLinkStyle = {
-    textTransform: "none",
-    _focus: { boxShadow: "none" },
-    fontSize: "2rem",
-    cursor: "pointer",
-    transition: "all 0.25s linear",
-    _hover: {
-        transform: "translateY(5px)",
-        color: "#fff",
-        "&::after": { content: "none" },
-    },
-    target: "_blank",
-    rel: "noopener noreferrer nofollow",
+  textTransform: "none",
+  _focus: { boxShadow: "none" },
+  fontSize: "2rem",
+  cursor: "pointer",
+  transition: "all 0.25s linear",
+  _hover: {
+    transform: "translateY(5px)",
+    color: "#fff",
+    "&::after": { content: "none" },
+  },
+  target: "_blank",
+  rel: "noopener noreferrer nofollow",
 };
