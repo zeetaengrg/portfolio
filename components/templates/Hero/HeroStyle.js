31a1edb03681aa7b6677cf7b9571cc820fafe92c
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-12 14:29:12 +0545

    style: hero section background image style modified

--- components/templates/Hero/HeroStyle.js
+++ components/templates/Hero/HeroStyle.js
@@ -1,60 +1,9 @@
-export const blobOneVariants = {
-  start: {
-    scale: 0.9,
-  },
-  finish: {
-    scale: 1.2,
-    transition: {
-      repeat: Infinity,
-      repeatType: "reverse",
-      duration: 3,
-    },
-  },
-};
-
-export const blobTwoVariants = {
-  start: {
-    scale: 0.8,
-  },
-  finish: {
-    scale: 1.3,
-    transition: {
-      delay: 1.5,
-      repeat: Infinity,
-      repeatType: "reverse",
-      duration: 3,
-    },
-  },
-};
-
-export const blobThreeVariants = {
-  start: {
-    scale: 0.8,
-  },
-  finish: {
-    scale: 1.2,
-    transition: {
-      delay: 3,
-      repeat: Infinity,
-      repeatType: "reverse",
-      duration: 3,
-    },
-  },
-};
-
-export const blobFourVariants = {
-  start: {
-    scale: 0.8,
-  },
-  finish: {
-    scale: 1.2,
-    transition: {
-      delay: 2.5,
-      repeat: Infinity,
-      repeatType: "reverse",
-      duration: 3,
-    },
-  },
+export const heroImgStyle = {
+  position: "absolute",
+  opacity: "0.3",
+  height: "89vh",
+  width: "100%",
+  objectFit: "cover",
 };
 
 export const commonVariants = {
@@ -71,95 +20,6 @@ export const commonVariants = {
       type: "spring",
       stiffness: 110,
     },
-  },
-};
-
-export const blobOneStyle = {
-  position: "absolute",
-  top: {
-    md: "20rem",
-    lg: "12rem",
-  },
-  left: {
-    md: "-5rem",
-    lg: "-10rem",
-  },
-  width: {
-    md: "50vw",
-  },
-  height: {
-    md: "40vh",
-    lg: "100vh",
-  },
-  opacity: "0.15",
-  display: {
-    base: "none",
-    md: "block",
-  },
-};
-
-export const blobTwoStyle = {
-  position: "absolute",
-  top: {
-    md: "3rem",
-    lg: "5rem",
-  },
-  right: {
-    md: "3rem",
-    lg: "5rem",
-  },
-  width: {
-    lg: "20vw",
-  },
-  height: {
-    md: "20vh",
-    lg: "40vh",
-  },
-  opacity: "0.5",
-  display: {
-    base: "none",
-    md: "block",
-  },
-};
-
-export const blobThreeStyle = {
-  position: "absolute",
-  top: {
-    md: "15rem",
-  },
-  right: {
-    md: "15rem",
-    lg: "25rem",
-  },
-  width: {
-    md: "20vw",
-  },
-  height: {
-    md: "20vh",
-    lg: "40vh",
-  },
-  opacity: "0.2",
-  display: {
-    base: "none",
-    md: "block",
-  },
-};
-
-export const blobFourStyle = {
-  position: "absolute",
-  top: "7rem",
-  left: "10rem",
-  width: {
-    md: "5vw",
-  },
-  height: {
-    md: "5vh",
-    lg: "15vh",
-  },
-  opacity: "0.5",
-  display: {
-    base: "none",
-    md: "block",
   },
 };
 
