b593e6cad13ba0bdb28cc2eacbae78e5881d1a02
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-12 08:58:56 +0545

    refactor: fragment removed

--- components/layouts/ThankYou/ThankYou.jsx
+++ components/layouts/ThankYou/ThankYou.jsx
@@ -5,20 +5,18 @@ import { flexStyle, headingStyle, textStyle, boxStyle } from "./ThankYouStyles";
 
 const ThankYou = () => {
   return (
-    <>
-      <Flex sx={flexStyle}>
-        <Box sx={boxStyle}>
-          <Heading sx={headingStyle}>Thank You!!!</Heading>
-          <Divider width="60%" />
-          <Text sx={textStyle}>
-            Your message has been sent successfully. I will get back to you
-            shortly.
-          </Text>
-          <FaPrayingHands style={{ fontSize: "2rem" }} />
-        </Box>
-        <GoBackBtn />
-      </Flex>
-    </>
+    <Flex sx={flexStyle}>
+      <Box sx={boxStyle}>
+        <Heading sx={headingStyle}>Thank You!!!</Heading>
+        <Divider width="60%" />
+        <Text sx={textStyle}>
+          Your message has been sent successfully. I will get back to you
+          shortly.
+        </Text>
+        <FaPrayingHands style={{ fontSize: "2rem" }} />
+      </Box>
+      <GoBackBtn />
+    </Flex>
   );
 };
 
