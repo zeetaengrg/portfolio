92b58cd1cb1910fac9c0adf336a37e4f5d29b3bf
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-14 01:35:55 +0545

    feat: markup for footer components modified

--- components/templates/Footer/Footer.jsx
+++ components/templates/Footer/Footer.jsx
@@ -1,31 +1,35 @@
-import { Center, Flex, Image, Box } from "@chakra-ui/react";
+import { Center, Flex, Image, Box, Text } from "@chakra-ui/react";
 import { FaCopyright } from "react-icons/fa";
-import { SocialIconLinks } from "@components/elements";
 import { boxStyle, imgTag, imageStyle } from "./FooterStyles";
 
 const Footer = () => {
+  const today = new Date();
+
   return (
     <Box sx={boxStyle}>
       <Image src="/images/wave.svg" alt="waves" sx={imgTag} />
-      <Flex justifyContent="center" direction="column" position="relative">
+      <Flex
+        justifyContent="center"
+        direction="column"
+        position="relative"
+        gap="1rem"
+      >
         <Center>
           <Image src="/images/logo.svg" alt="Logo" sx={imageStyle} />
         </Center>
-        <Flex
-          m={{ base: "1rem 0", md: "0.5rem 0" }}
-          gap="2rem"
-          justifyContent="center"
-        >
-          <SocialIconLinks />
-        </Flex>
-        <Box
-          fontSize="0.8rem"
-          mt={{ base: "0.5rem", md: "1rem" }}
-          mb={{ base: "1rem", md: "1.5rem" }}
-        >
-          <Flex justifyContent="center" alignItems="center">
-            2022 <FaCopyright style={{ margin: "0 0.5rem" }} />
-            Jiten Gurung. All Rights Reserved.
+        <Box fontSize="0.8rem" mb={{ base: "1rem", md: "2.5rem" }}>
+          <Flex
+            justifyContent="center"
+            alignItems="center"
+            direction="column"
+            gap="0.5rem"
+          >
+            <Text>Designed & Built By Jiten Gurung</Text>
+            <Flex alignItems="center">
+              {today.getFullYear()}{" "}
+              <FaCopyright style={{ margin: "0 0.5rem" }} />
+              All Rights Reserved.
+            </Flex>
           </Flex>
         </Box>
       </Flex>
