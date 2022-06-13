cb4553402250ccfe48ba2c97ae98813d37d54ee5
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-14 01:36:14 +0545

    refactor: style rules refactored

--- components/templates/Footer/FooterStyles.js
+++ components/templates/Footer/FooterStyles.js
@@ -1,24 +1,26 @@
 export const boxStyle = {
-    backgroundColor: {
-        base: "#141e39",
-        md: "transparent",
-    },
-}
+  marginTop: "5rem",
+  backgroundColor: {
+    base: "#141e39",
+    md: "transparent",
+  },
+};
 
 export const imgTag = {
-    position: "absolute",
-    bottom: "0",
-    width: "100vw",
-    display: {
-        base: "none",
-        md: "block"
-    }
-}
+  position: "absolute",
+  bottom: "0",
+  width: "100vw",
+  display: {
+    base: "none",
+    md: "block",
+  },
+};
 
 export const imageStyle = {
-    width: "60px",
-    height: "45px",
-    margin: {
-        base: "1rem 0", md: "2rem 0"
-    }
-}
+  width: "60px",
+  height: "45px",
+  margin: {
+    base: "1rem 0",
+    md: "1rem 0",
+  },
+};
