ec447d5c00be8a37ec21ac3a5528a5dd2c0d1121
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-13 15:20:29 +0545

    refactor: styled objects refactored

--- components/templates/Navbar/NavbarStyles.js
+++ components/templates/Navbar/NavbarStyles.js
@@ -1,34 +1,34 @@
-export const linkVariants = {
-    hidden: {
-        opacity: 0,
-        y: -20,
-    },
-    visible: i => ({
-        opacity: 1,
-        y: 0,
-        transition: {
-            delay: i * 0.2,
-            duration: 1,
-            type: "spring",
-            stiffness: 110,
-        }
-    })
-}
-
 export const linkItem = ["About Me", "Skills", "Projects", "Contacts"];
 
+export const linkVariants = {
+  hidden: {
+    opacity: 0,
+    y: -20,
+  },
+  visible: (i) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: i * 0.2,
+      duration: 1,
+      type: "spring",
+      stiffness: 110,
+    },
+  }),
+};
+
 export const linkStyle = {
-    _hover: {
-        textDecoration: "none",
-    },
-    fontSize: {
-        base: "0.72rem",
-        md: "0.9rem",
-    },
-    _focus: { boxShadow: "none" },
-    display: {
-        base: "none",
-        md: "block",
-        lg: "block",
-    }
+  _hover: {
+    textDecoration: "none",
+  },
+  fontSize: {
+    base: "0.72rem",
+    md: "0.9rem",
+  },
+  _focus: { boxShadow: "none" },
+  display: {
+    base: "none",
+    md: "block",
+    lg: "block",
+  },
 };
