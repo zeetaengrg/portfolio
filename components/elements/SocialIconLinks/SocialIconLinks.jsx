f589d314b4105fcb1381a7efd8effcde6b2cd8f6
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-14 14:11:44 +0545

    refactor: indentation modified

--- components/elements/SocialIconLinks/SocialIconLinks.jsx
+++ components/elements/SocialIconLinks/SocialIconLinks.jsx
@@ -1,27 +1,35 @@
-import { Box, Link } from "@chakra-ui/react";
-import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare } from "react-icons/fa";
+import { Box, Flex, Link } from "@chakra-ui/react";
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaTwitterSquare,
+} from "react-icons/fa";
 import { linkStyle } from "../LinkStyle";
 import { links, iconStyle } from "./SocialIconsLinksStyles";
 
 const SocialIconLinks = () => {
-
-    return (
-        <>
-            {links.map(link => (
-                <Link href={link.url} aria-label={link.name} key={link.id} {...linkStyle}>
-                    <Box {...iconStyle}>
-                        {link.name === "Facebook Icon" ? (
-                            <FaFacebookSquare />
-                        ) : link.name === "Instagram Icon" ? (
-                            <FaInstagramSquare />
-                        ) : (
-                            <FaTwitterSquare />
-                        )}
-                    </Box>
-                </Link>
-            ))}
-        </>
-    );
+  return (
+    <Flex justifyContent="center" alignItems="center" gap="1rem">
+      {links.map((link) => (
+        <Link
+          href={link.url}
+          aria-label={link.name}
+          key={link.id}
+          {...linkStyle}
+        >
+          <Box {...iconStyle}>
+            {link.name === "Facebook Icon" ? (
+              <FaFacebookSquare />
+            ) : link.name === "Instagram Icon" ? (
+              <FaInstagramSquare />
+            ) : (
+              <FaTwitterSquare />
+            )}
+          </Box>
+        </Link>
+      ))}
+    </Flex>
+  );
 };
 
 export default SocialIconLinks;
