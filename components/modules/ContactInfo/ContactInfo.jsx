38f41181f3a8c0345db21c07d726b9b3c48d97a7
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-14 14:14:18 +0545

    refactor: indentation modified

--- components/modules/ContactInfo/ContactInfo.jsx
+++ components/modules/ContactInfo/ContactInfo.jsx
@@ -17,71 +17,69 @@ const MotionFlex = motion(Flex);
 
 const ContactInfo = () => {
   return (
-    <>
-      <Box>
-        <MotionFlex
-          flexDirection="column"
-          justifyContent="center"
-          gap={{ base: "1rem", md: "2rem" }}
-          alignItems={{ base: "center" }}
-          variants={commonVariants}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          transition={{
-            delay: 0.2,
-            duration: 0.5,
-            type: "tween",
-          }}
-        >
-          <Box>
-            <Flex
-              direction="column"
-              justifyContent="center"
-              gap={{ base: "1rem", md: "2rem" }}
-              alignItems={{ base: "center", md: "flex-start" }}
-            >
-              <Flex sx={flexStyle}>
-                <ImLocation style={iconStyle} />
-                <Text sx={textStyle}>Pokhara, Nepal</Text>
-              </Flex>
-              <Flex sx={flexStyle}>
-                <FaMobileAlt style={iconStyle} />
-                <Text sx={textStyle}>+977 9803709974</Text>
-              </Flex>
-              <Flex sx={flexStyle}>
-                <HiMail style={iconStyle} />
-                <Link href="mailto:zeetaen.gurung@gmail.com" sx={mailStyle}>
-                  <Text sx={textStyle}>zeetaen.gurung@gmail.com</Text>
-                </Link>
-              </Flex>
+    <Box as="section" display={{ base: "block", md: "none" }}>
+      <MotionFlex
+        flexDirection="column"
+        justifyContent="center"
+        gap={{ base: "1rem", md: "2rem" }}
+        alignItems={{ base: "center" }}
+        variants={commonVariants}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{
+          delay: 0.2,
+          duration: 0.5,
+          type: "tween",
+        }}
+      >
+        <Box>
+          <Flex
+            direction="column"
+            justifyContent="center"
+            gap={{ base: "1rem", md: "2rem" }}
+            alignItems={{ base: "center", md: "flex-start" }}
+          >
+            <Flex sx={flexStyle}>
+              <ImLocation style={iconStyle} />
+              <Text sx={textStyle}>Pokhara, Nepal</Text>
             </Flex>
-          </Box>
-          <Divider />
-          <Box>
-            <Flex gap="5rem">
-              {links.map((link) => (
-                <Link
-                  href={link.url}
-                  aria-label={link.name}
-                  key={link.name}
-                  {...socialLinkStyle}
-                >
-                  {link.name === "LinkedIn Icon" ? (
-                    <FaLinkedinIn />
-                  ) : link.name === "Github Icon" ? (
-                    <FaGithub />
-                  ) : (
-                    <FaCodepen />
-                  )}
-                </Link>
-              ))}
+            <Flex sx={flexStyle}>
+              <FaMobileAlt style={iconStyle} />
+              <Text sx={textStyle}>+977 9803709974</Text>
             </Flex>
-          </Box>
-          <Divider />
-        </MotionFlex>
-      </Box>
-    </>
+            <Flex sx={flexStyle}>
+              <HiMail style={iconStyle} />
+              <Link href="mailto:zeetaen.gurung@gmail.com" sx={mailStyle}>
+                <Text sx={textStyle}>zeetaen.gurung@gmail.com</Text>
+              </Link>
+            </Flex>
+          </Flex>
+        </Box>
+        <Divider />
+        <Box>
+          <Flex gap="5rem">
+            {links.map((link) => (
+              <Link
+                href={link.url}
+                aria-label={link.name}
+                key={link.name}
+                {...socialLinkStyle}
+              >
+                {link.name === "LinkedIn Icon" ? (
+                  <FaLinkedinIn />
+                ) : link.name === "Github Icon" ? (
+                  <FaGithub />
+                ) : (
+                  <FaCodepen />
+                )}
+              </Link>
+            ))}
+          </Flex>
+        </Box>
+        <Divider />
+      </MotionFlex>
+    </Box>
   );
 };
 
