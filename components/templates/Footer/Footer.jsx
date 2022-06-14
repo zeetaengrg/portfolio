baf140318fc89dc695e14510469e4cfa4c644121
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-14 14:16:08 +0545

    feat: SocialIconLinks component imported and added to the markup

--- components/templates/Footer/Footer.jsx
+++ components/templates/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import { Center, Flex, Image, Box, Text } from "@chakra-ui/react";
+import { SocialIconLinks } from "@components/elements";
 import { FaCopyright } from "react-icons/fa";
 import { boxStyle, imgTag, imageStyle } from "./FooterStyles";
 
@@ -12,17 +13,20 @@ const Footer = () => {
         justifyContent="center"
         direction="column"
         position="relative"
-        gap="1rem"
+        gap={{ base: "1rem", md: "1rem", lg: "2rem" }}
       >
         <Center>
           <Image src="/images/logo.svg" alt="Logo" sx={imageStyle} />
         </Center>
-        <Box fontSize="0.8rem" mb={{ base: "1rem", md: "2.5rem" }}>
+        <Box display={{ base: "block", md: "none" }}>
+          <SocialIconLinks />
+        </Box>
+        <Box fontSize="0.8rem" mb={{ base: "0.5rem", lg: "1.5rem" }}>
           <Flex
             justifyContent="center"
             alignItems="center"
             direction="column"
-            gap="0.5rem"
+            gap={{ base: "0rem", lg: "0.5rem" }}
           >
             <Text>Designed & Built By Jiten Gurung</Text>
             <Flex alignItems="center">
