540d81a5fc67f2a446a7e352a8603a9e852a4106
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-14 14:12:12 +0545

    refactor: indentation modified

--- components/elements/SocialIconLinks/SocialIconsLinksStyles.js
+++ components/elements/SocialIconLinks/SocialIconsLinksStyles.js
@@ -1,23 +1,23 @@
 export const links = [
-    {
-        id: 1,
-        name: "Facebook Icon",
-        url: "https://www.facebook.com/zeetaen/"
-    },
-    {
-        id: 2,
-        name: "Instagram Icon",
-        url: "https://www.instagram.com/zeetaen/"
-    },
-    {
-        id: 3,
-        name: "Twitter Icon",
-        url: "https://twitter.com/zeetaen"
-    }
-]
+  {
+    id: 1,
+    name: "Facebook Icon",
+    url: "https://www.facebook.com/zeetaen/",
+  },
+  {
+    id: 2,
+    name: "Instagram Icon",
+    url: "https://www.instagram.com/zeetaen/",
+  },
+  {
+    id: 3,
+    name: "Twitter Icon",
+    url: "https://twitter.com/zeetaen",
+  },
+];
 
 export const iconStyle = {
-    fontSize: "1.5rem",
-    transition: "all 0.2s linear",
-    _hover: { color: "white", transform: "translateY(-2px)" },
-}
+  fontSize: "1.5rem",
+  transition: "all 0.2s linear",
+  _hover: { color: "white", transform: "translateY(-2px)" },
+};
