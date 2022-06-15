9596094ad0c10803da243f2fc4cb772da5c52f75
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-15 19:44:50 +0545

    feat: margin for button modified

--- components/elements/Btn/GoBackBtn/GoBackBtn.jsx
+++ components/elements/Btn/GoBackBtn/GoBackBtn.jsx
@@ -1,31 +1,32 @@
-import { Link, Button } from '@chakra-ui/react';
-import { RiArrowGoBackLine } from 'react-icons/ri';
-import { btnStyle } from '../Btn';
+import { Link, Button } from "@chakra-ui/react";
+import { RiArrowGoBackLine } from "react-icons/ri";
+import { btnStyle } from "../Btn";
 
 const GoBackBtn = () => {
+  const linkStyle = {
+    _hover: {
+      "&::after": { content: "none" },
+      color: "#a5abbd",
+    },
+    textTransform: "none",
+    _focus: { outline: "none" },
+  };
 
-    const linkStyle = {
-        _hover: {
-            "&::after": { content: "none" },
-            color: "#a5abbd",
-        },
-        textTransform: "none",
-        _focus: { boxShadow: "none" },
-    };
-
-    return (
-        <>
-            <Link sx={linkStyle} href="/">
-                <Button
-                    m={{ base: "2.69rem 0", md: "1.5rem 0" }}
-                    sx={btnStyle}
-                    rightIcon={<RiArrowGoBackLine />}
-                >
-                    Go Back
-                </Button>
-            </Link>
-        </>
-    );
+  return (
+    <Link sx={linkStyle} href="/" position="relative">
+      <Button
+        m={{ base: "1rem 0", md: "1rem 0rem", lg: "0rem" }}
+        sx={btnStyle}
+        rightIcon={<RiArrowGoBackLine />}
+        position="absolute"
+        zIndex="1"
+        left="50%"
+        transform="translateX(-50%)"
+      >
+        Go Back
+      </Button>
+    </Link>
+  );
 };
 
 export default GoBackBtn;
