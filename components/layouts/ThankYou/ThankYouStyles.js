a1910ed0581c34f550a3eb0bf7be1e84c4f36497
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-15 19:49:07 +0545

    refactor: code prettified

--- components/layouts/ThankYou/ThankYouStyles.js
+++ components/layouts/ThankYou/ThankYouStyles.js
@@ -1,44 +1,46 @@
 export const flexStyle = {
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    height: {
-        base: "68.9vh",
-        md: "66.5vh",
-        lg: "66.5vh"
-    },
-}
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+  gap: {
+    base: "1rem",
+    md: "2rem",
+  },
+  margin: "1rem auto 3rem",
+  height: {
+    base: "60vh",
+    md: "72.5vh",
+    lg: "76vh",
+  },
+};
 
 export const headingStyle = {
-    color: "#0B111E",
-    fontWeight: "700",
-    textTransform: "uppercase",
-}
+  color: "#0B111E",
+  fontWeight: "700",
+  textTransform: "uppercase",
+};
 
 export const boxStyle = {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    background: "#384765",
-    width: {
-        base: "80%",
-        md: "40%"
-    },
-    padding: {
-        base: "0.5rem",
-        md: "1.5rem"
-    },
-    borderRadius: "0.5rem",
-    margin: {
-        base: "1rem 0",
-        md: "1.5rem 0"
-    }
-}
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  background: "#384765",
+  width: {
+    base: "80%",
+    md: "60%",
+    lg: "50%",
+  },
+  padding: {
+    base: "0.5rem",
+    md: "1rem",
+  },
+  borderRadius: "0.5rem",
+};
 
 export const textStyle = {
-    fontSize: "1.2rem",
-    color: "#fff",
-    margin: "1rem",
-    textAlign: "center",
-}
+  fontSize: "1.2rem",
+  color: "#fff",
+  margin: "1rem",
+  textAlign: "center",
+};
