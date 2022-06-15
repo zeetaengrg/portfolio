8e499c2eb734f19367a6182904626bd311a807e3
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-15 14:31:31 +0545

    refactor: semantic html tag added

--- components/modules/ContactForm/ContactForm.jsx
+++ components/modules/ContactForm/ContactForm.jsx
@@ -1,8 +1,8 @@
+import { motion } from "framer-motion";
 import { useRef, useState } from "react";
 import { useRouter } from "next/router";
 import emailjs from "@emailjs/browser";
 import { RiSendPlaneFill } from "react-icons/ri";
-import { motion } from "framer-motion";
 import { VStack, FormLabel, Input, Textarea, Button } from "@chakra-ui/react";
 import { commonVariants, inputStyle, btn } from "./ContactFormStyles";
 
@@ -43,74 +43,73 @@ const ContactForm = () => {
   };
 
   return (
-    <>
-      <form ref={formRef} onSubmit={sendEmail}>
-        <MotionVStack
-          spacing={2}
-          alignItems="flex-start"
-          variants={commonVariants}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          transition={{
-            delay: 0.2,
-            duration: 0.5,
-            type: "tween",
-          }}
+    <form ref={formRef} onSubmit={sendEmail}>
+      <MotionVStack
+        as="article"
+        spacing={2}
+        alignItems="flex-start"
+        variants={commonVariants}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{
+          delay: 0.2,
+          duration: 0.5,
+          type: "tween",
+        }}
+      >
+        <FormLabel htmlFor="fullName" mb="0rem">
+          Full Name
+        </FormLabel>
+        <Input
+          sx={inputStyle}
+          type="text"
+          placeholder="Insert Your Full Name..."
+          id="fullName"
+          name="fullName"
+          required
+        />
+        <FormLabel htmlFor="email">Email Address</FormLabel>
+        <Input
+          sx={inputStyle}
+          placeholder="Insert Your Email..."
+          name="email"
+          id="email"
+          type="email"
+          required
+        />
+        <FormLabel htmlFor="subject">Subject</FormLabel>
+        <Input
+          sx={inputStyle}
+          placeholder="Insert Your Subject..."
+          name="subject"
+          id="subject"
+          type="text"
+          required
+        />
+        <FormLabel htmlFor="message">Message</FormLabel>
+        <Textarea
+          sx={inputStyle}
+          placeholder="Insert Your Message..."
+          name="message"
+          id="message"
+          height="10rem"
+          required
+        />
+        <Button
+          mt="1rem"
+          sx={btn}
+          type="submit"
+          rightIcon={<RiSendPlaneFill style={{ fontSize: "1.5rem" }} />}
+          onClick={handleSend}
+          isLoading={isSending}
+          loadingText="Sending..."
+          spinnerPlacement="end"
         >
-          <FormLabel htmlFor="fullName" mb="0rem">
-            Full Name
-          </FormLabel>
-          <Input
-            sx={inputStyle}
-            type="text"
-            placeholder="Insert Your Full Name"
-            id="fullName"
-            name="fullName"
-            required
-          />
-          <FormLabel htmlFor="email">Email Address</FormLabel>
-          <Input
-            sx={inputStyle}
-            placeholder="Insert Your Email..."
-            name="email"
-            id="email"
-            type="email"
-            required
-          />
-          <FormLabel htmlFor="subject">Subject</FormLabel>
-          <Input
-            sx={inputStyle}
-            placeholder="Insert Your Subject..."
-            name="subject"
-            id="subject"
-            type="text"
-            required
-          />
-          <FormLabel htmlFor="message">Message</FormLabel>
-          <Textarea
-            sx={inputStyle}
-            placeholder="Insert Your Message..."
-            name="message"
-            id="message"
-            height="10rem"
-            required
-          />
-          <Button
-            mt="1rem"
-            sx={btn}
-            type="submit"
-            rightIcon={<RiSendPlaneFill style={{ fontSize: "1.5rem" }} />}
-            onClick={handleSend}
-            isLoading={isSending}
-            loadingText="Sending..."
-            spinnerPlacement="end"
-          >
-            Send
-          </Button>
-        </MotionVStack>
-      </form>
-    </>
+          Send
+        </Button>
+      </MotionVStack>
+    </form>
   );
 };
 
