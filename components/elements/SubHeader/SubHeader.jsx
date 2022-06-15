6e93a005ad2f7be2e4c9c8a5b31e3f3435ad4085
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-15 14:29:33 +0545

    feat: markup for heading element modified for web accessiblity

--- components/elements/SubHeader/SubHeader.jsx
+++ components/elements/SubHeader/SubHeader.jsx
@@ -1,36 +1,36 @@
 import { motion } from "framer-motion";
-import { Center, Text } from "@chakra-ui/react";
+import { Center, Heading } from "@chakra-ui/react";
 
-const MotionText = motion(Text)
+const MotionHeading = motion(Heading);
 
 const commonVariants = {
-	hidden: {
-        opacity: 0,
-        y: "5vh",
-    },
-    visible: {
-        opacity: 1,
-        y: 0,
-    }
+  hidden: {
+    opacity: 0,
+    y: "5vh",
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
 };
 
 const SubHeader = ({ subHeading }) => {
-
-    return (
-        <Center>
-            <MotionText
-                fontWeight="bold"
-                fontSize="1.1rem"
-                variants={commonVariants}
-                initial="hidden"
-                whileInView="visible"
-                transition={{ delay: 0.3, duration: 0.5, type: "tween" }}
-                viewport={{ once: true }}
-            >
-                {subHeading}
-            </MotionText>
-        </Center>
-    )
-}
+  return (
+    <Center as="section">
+      <MotionHeading
+        as="h2"
+        fontWeight="bold"
+        fontSize="1.1rem"
+        variants={commonVariants}
+        initial="hidden"
+        whileInView="visible"
+        transition={{ delay: 0.3, duration: 0.5, type: "tween" }}
+        viewport={{ once: true }}
+      >
+        {subHeading}
+      </MotionHeading>
+    </Center>
+  );
+};
 
 export default SubHeader;
