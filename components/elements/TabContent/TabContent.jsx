e7f0fa9d1d83117729c69ab44d938b83486c0827
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-15 19:48:27 +0545

    feat: Image component from chakra ui used instead of nextjs Image component

--- components/elements/TabContent/TabContent.jsx
+++ components/elements/TabContent/TabContent.jsx
@@ -1,16 +1,15 @@
-import Image from "next/image";
-import { Tooltip } from "@chakra-ui/react";
+import { Image, Tooltip } from "@chakra-ui/react";
 
 const TabContent = ({ item }) => {
   return (
-    <Tooltip as="figcaption" key={item.id} label={item.name} placement="top">
-      <Image
-        src={item.image}
-        alt={item.name}
-        height="75"
-        width="75"
-        objectFit="contain"
-      />
+    <Tooltip as="figcaption" label={item.name} placement="top">
+    <Image
+      src={item.image}
+      alt={item.name}
+      height="75"
+      width="75"
+      objectFit="contain"
+    />
     </Tooltip>
   );
 };
