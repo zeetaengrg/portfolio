0d16dc59a695504ed3234c7bdee8191f855a3fc2
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-15 14:16:46 +0545

    fix: transition issue fixed

--- components/elements/Btn/Btn.js
+++ components/elements/Btn/Btn.js
@@ -2,11 +2,11 @@ export const btnStyle = {
   padding: "0 0.8rem",
   backgroundColor: "#141E39",
   fontSize: "1.2rem",
+  transition: "all 0.3s ease",
   _focus: { boxShadow: "none" },
   _hover: {
     bg: "#a5abbd",
     color: "#0b111e",
-    transition: "all 0.3s ease-in-out",
   },
   _active: { bg: "#a5abbd", color: "#0b111e" },
   boxShadow:
