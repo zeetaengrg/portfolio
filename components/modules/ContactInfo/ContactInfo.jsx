43c9bd029a960e9912f248e4dd0c4bd9d2ec2019
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-15 14:32:22 +0545

    refactor: semantic html tag added and markup for social media icons list modified

--- components/modules/ContactInfo/ContactInfo.jsx
+++ components/modules/ContactInfo/ContactInfo.jsx
@@ -1,8 +1,16 @@
-import { Box, Flex, Text, Link, Divider } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import {
+  Box,
+  Flex,
+  Text,
+  Link,
+  Divider,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
 import { ImLocation } from "react-icons/im";
 import { FaMobileAlt, FaLinkedinIn, FaGithub, FaCodepen } from "react-icons/fa";
 import { HiMail } from "react-icons/hi";
-import { motion } from "framer-motion";
 import {
   commonVariants,
   links,
@@ -17,69 +25,63 @@ const MotionFlex = motion(Flex);
 
 const ContactInfo = () => {
   return (
-    <Box as="section" display={{ base: "block", md: "none" }}>
-      <MotionFlex
-        flexDirection="column"
+    <MotionFlex
+      as="article"
+      display={{ base: "block", md: "none" }}
+      flexDirection="column"
+      justifyContent="center"
+      gap={{ base: "1rem", md: "2rem" }}
+      alignItems={{ base: "center" }}
+      variants={commonVariants}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{
+        delay: 0.2,
+        duration: 0.5,
+        type: "tween",
+      }}
+    >
+      <Flex
+        as="section"
+        direction="column"
         justifyContent="center"
         gap={{ base: "1rem", md: "2rem" }}
-        alignItems={{ base: "center" }}
-        variants={commonVariants}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true }}
-        transition={{
-          delay: 0.2,
-          duration: 0.5,
-          type: "tween",
-        }}
+        alignItems={{ base: "center", md: "flex-start" }}
       >
-        <Box>
-          <Flex
-            direction="column"
-            justifyContent="center"
-            gap={{ base: "1rem", md: "2rem" }}
-            alignItems={{ base: "center", md: "flex-start" }}
-          >
-            <Flex sx={flexStyle}>
-              <ImLocation style={iconStyle} />
-              <Text sx={textStyle}>Pokhara, Nepal</Text>
-            </Flex>
-            <Flex sx={flexStyle}>
-              <FaMobileAlt style={iconStyle} />
-              <Text sx={textStyle}>+977 9803709974</Text>
-            </Flex>
-            <Flex sx={flexStyle}>
-              <HiMail style={iconStyle} />
-              <Link href="mailto:zeetaen.gurung@gmail.com" sx={mailStyle}>
-                <Text sx={textStyle}>zeetaen.gurung@gmail.com</Text>
-              </Link>
-            </Flex>
-          </Flex>
-        </Box>
-        <Divider />
-        <Box>
-          <Flex gap="5rem">
-            {links.map((link) => (
-              <Link
-                href={link.url}
-                aria-label={link.name}
-                key={link.name}
-                {...socialLinkStyle}
-              >
-                {link.name === "LinkedIn Icon" ? (
-                  <FaLinkedinIn />
-                ) : link.name === "Github Icon" ? (
-                  <FaGithub />
-                ) : (
-                  <FaCodepen />
-                )}
-              </Link>
-            ))}
-          </Flex>
-        </Box>
-        <Divider />
-      </MotionFlex>
-    </Box>
+        <Flex sx={flexStyle}>
+          <ImLocation style={iconStyle} />
+          <Text sx={textStyle}>Pokhara, Nepal</Text>
+        </Flex>
+        <Flex sx={flexStyle}>
+          <FaMobileAlt style={iconStyle} />
+          <Text sx={textStyle}>+977 9803709974</Text>
+        </Flex>
+        <Flex sx={flexStyle}>
+          <HiMail style={iconStyle} />
+          <Link href="mailto:zeetaen.gurung@gmail.com" sx={mailStyle}>
+            <Text sx={textStyle}>zeetaen.gurung@gmail.com</Text>
+          </Link>
+        </Flex>
+      </Flex>
+      <Divider as="figure" m="1rem 0rem" />
+      <List display="flex" gap="5rem">
+        {links.map((link) => (
+          <ListItem key={link.id} m="0 auto">
+            <Link href={link.url} aria-label={link.name} {...socialLinkStyle}>
+              {link.name === "LinkedIn Icon" ? (
+                <FaLinkedinIn />
+              ) : link.name === "Github Icon" ? (
+                <FaGithub />
+              ) : (
+                <FaCodepen />
+              )}
+            </Link>
+          </ListItem>
+        ))}
+      </List>
+      <Divider as="figure" m="1rem 0rem" />
+    </MotionFlex>
   );
 };
 
