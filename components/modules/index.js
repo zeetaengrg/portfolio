c0447aae4e79a7cc09e89cb15846234d4f4df8dd
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-16 13:14:02 +0545

    feat: named exports for AboutMeText component added

--- components/modules/index.js
+++ components/modules/index.js
@@ -1,2 +1,3 @@
 export { default as ContactForm } from "./ContactForm/ContactForm";
 export { default as ContactInfo } from "./ContactInfo/ContactInfo";
+export { default as AboutMeText } from "./AboutMeText/AboutMeText";
