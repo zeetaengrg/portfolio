3a7427a2d617c4fde292804dc954a28996644642
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-18 11:40:56 +0545

    feat: go back btn animation added

--- components/elements/Btn/GoBackBtn/GoBackBtn.jsx
+++ components/elements/Btn/GoBackBtn/GoBackBtn.jsx
@@ -1,8 +1,25 @@
+import { motion } from "framer-motion";
 import { Link, Button } from "@chakra-ui/react";
 import { RiArrowGoBackLine } from "react-icons/ri";
 import { btnStyle } from "../Btn";
 
 const GoBackBtn = () => {
+  const btnVariant = {
+    start: {
+      opacity: 0,
+      y: 20,
+    },
+    end: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 1,
+        duration: 0.8,
+        ease: "easeIn",
+      },
+    },
+  };
+
   const linkStyle = {
     _hover: {
       "&::after": { content: "none" },
@@ -15,13 +32,13 @@ const GoBackBtn = () => {
   return (
     <Link sx={linkStyle} href="/" position="relative">
       <Button
+        as={motion.btn}
+        variants={btnVariant}
+        initial="start"
+        animate="end"
         m={{ base: "1rem 0", md: "1rem 0rem", lg: "0rem" }}
         sx={btnStyle}
         rightIcon={<RiArrowGoBackLine />}
-        position="absolute"
-        zIndex="1"
-        left="50%"
-        transform="translateX(-50%)"
       >
         Go Back
       </Button>
