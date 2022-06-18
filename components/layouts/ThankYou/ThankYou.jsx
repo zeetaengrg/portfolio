93ef4de4ec4bd28d0d19856f44a4e63cb7a54e76
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-18 11:43:35 +0545

    feta: motion animation added to thank you page container

--- components/layouts/ThankYou/ThankYou.jsx
+++ components/layouts/ThankYou/ThankYou.jsx
@@ -1,12 +1,25 @@
+import { motion } from "framer-motion";
 import { Box, Divider, Flex, Heading, Text } from "@chakra-ui/react";
 import { FaPrayingHands } from "react-icons/fa";
 import { GoBackBtn } from "@components/elements";
-import { flexStyle, headingStyle, textStyle, boxStyle } from "./ThankYouStyles";
+import {
+  flexStyle,
+  headingStyle,
+  textStyle,
+  boxStyle,
+  boxVariants,
+} from "./ThankYouStyles";
 
 const ThankYou = () => {
   return (
     <Flex as="section" sx={flexStyle}>
-      <Box as="article" sx={boxStyle}>
+      <Box
+        as={motion.article}
+        sx={boxStyle}
+        variants={boxVariants}
+        initial="start"
+        animate="end"
+      >
         <Heading as="h1" sx={headingStyle}>
           Thank You!!!
         </Heading>
