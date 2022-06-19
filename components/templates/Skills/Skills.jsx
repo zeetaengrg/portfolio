63e6188b83313519bcc1c4b06df8afbceaa0cbac
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-19 10:07:21 +0545

    fix: height issue for xl screen size fixed

--- components/templates/Skills/Skills.jsx
+++ components/templates/Skills/Skills.jsx
@@ -34,7 +34,7 @@ const Skills = () => {
     <Box
       as="section"
       id="skills"
-      height={{ base: "65vh", md: "60vh", lg: "45vh" }}
+      height={{ base: "65vh", md: "60vh", lg: "45vh", xl: "100vh" }}
       display="flex"
       flexDirection="column"
       justifyContent="center"
