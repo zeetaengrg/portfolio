71397adecc7b98c8821d7895e66d0e324271e25e
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-27 11:17:51 +0545

    fix: outline property used instead of boxShadow

--- components/elements/LinkStyle.js
+++ components/elements/LinkStyle.js
@@ -1,10 +1,10 @@
 export const linkStyle = {
-    _hover: {
-        "&::after": { content: "none" },
-        color: "#a5abbd",
-        textDecoration: "none",
-    },
-    _focus: { boxShadow: "none" },
-    target: "_blank",
-    rel: "noopener noreferrer nofollow",
-}
+  _hover: {
+    "&::after": { content: "none" },
+    color: "#a5abbd",
+    textDecoration: "none",
+  },
+  _focus: { outline: "none" },
+  target: "_blank",
+  rel: "noopener noreferrer nofollow",
+};
