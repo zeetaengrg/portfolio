2411f181052a79f721a1011ca3639515ef9edbcf
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-07-02 14:34:30 +0545

    feat: new SkillsTable component and new images for projects section added

---
+++ components/elements/SkillsTable/SkillsTable.jsx
@@ -0,0 +1,37 @@
+import { Box } from "@chakra-ui/react";
+import { gridStyle } from "@components/modules/ProjectsStyle";
+
+const SkillsTable = ({ project, isReverse }) => {
+  const tableStyle = {
+    width: "100%",
+    marginTop: "0.5rem",
+  };
+
+  const dataStyle = {
+    border: "0.25px groove rgba(165,171,189,0.15)",
+    borderCollapse: "collapse",
+    padding: "0.25rem 0.5rem",
+  };
+
+  return (
+    <table style={tableStyle}>
+      <tbody>
+        <tr style={gridStyle}>
+          {isReverse
+            ? project.skillsTwo.map((skill) => (
+                <td key={skill} style={dataStyle}>
+                  <Box textAlign={{ base: "center", lg: "left" }}>{skill}</Box>
+                </td>
+              ))
+            : project.skillsOne.map((skill) => (
+                <td key={skill} style={dataStyle}>
+                  <Box textAlign={{ base: "center", lg: "right" }}>{skill}</Box>
+                </td>
+              ))}
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default SkillsTable;
--- styles/theme.jsx
+++ styles/theme.jsx
@@ -1,43 +1,43 @@
 import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
-    styles: {
-        global: {
-            'html, body' : {
-                backgroundColor: '#0B111E',
-                color: '#a5abbd',
-                fontFamily: '"Poppins", sans-serif',
-                scrollBehavior: 'smooth',
-            },
-            
-            a : {
-                textTransform: 'uppercase',
-                fontSize: '0.9rem',
-                fontWeight: '500',
-                position: 'relative',
-                _hover: {
-                    textDecoration: 'none',
-                    color: '#fff',
-                },
+  styles: {
+    global: {
+      "html, body": {
+        backgroundColor: "#0B111E",
+        color: "#a5abbd",
+        fontFamily: '"Poppins", sans-serif',
+        scrollBehavior: "smooth",
+      },
 
-                '&::after' : {
-                    content: '" "',
-                    position: 'absolute',
-                    left: '0',
-                    bottom: '0',
-                    width: '100%',
-                    height: '1px',
-                    background: 'white',
-                    transform: 'scaleX(0)',
-                    transformOrigin: 'center',
-                    transition: 'transform 0.2s ease-in-out',
-                },
+      a: {
+        textTransform: "uppercase",
+        fontSize: "0.9rem",
+        fontWeight: "500",
+        position: "relative",
+        _hover: {
+          textDecoration: "none",
+          color: "#fff",
+        },
 
-                '&:hover::after' : {
-                    transform: 'scaleX(1)',
-                    transformOrigin: 'center',
-                }
-            }
-        }
-    }
+        "&::after": {
+          content: '" "',
+          position: "absolute",
+          left: "0",
+          bottom: "0",
+          width: "100%",
+          height: "1px",
+          background: "white",
+          transform: "scaleX(0)",
+          transformOrigin: "center",
+          transition: "transform 0.2s ease-in-out",
+        },
+
+        "&:hover::after": {
+          transform: "scaleX(1)",
+          transformOrigin: "center",
+        },
+      },
+    },
+  },
 });
