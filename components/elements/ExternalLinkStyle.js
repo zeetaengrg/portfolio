34c3e7fa8fc949d53a0676459bdb0210740d39c2
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-19 19:06:09 +0545

    feat: new styled objects files added for BtnLinkStyle, BtnStyle, and ExternalLinkStyle

---
+++ components/elements/ExternalLinkStyle.js
@@ -0,0 +1,9 @@
+export const externalLinkStyle = {
+  color: "#3892f9",
+  _active: { outline: "none" },
+  _focus: { outline: "none" },
+  textTransform: "none",
+  textDecoration: "underline",
+  fontSize: "1rem",
+  fontWeight: "normal",
+};
