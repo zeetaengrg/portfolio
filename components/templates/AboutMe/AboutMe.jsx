c47eb5a76f8f060e93777bebdb21924141a72209
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-26 18:15:11 +0545

    style: height and flex modified

--- components/templates/AboutMe/AboutMe.jsx
+++ components/templates/AboutMe/AboutMe.jsx
@@ -13,7 +13,10 @@ const AboutMe = () => {
     <Box
       as="section"
       display="flex"
+      justifyContent="center"
+      alignItems="center"
       flexDirection="column"
+      height={{ base: "100%", md: "70vh", lg: "100vh" }}
       id="aboutme"
       mt={{ base: "2rem", md: "0rem" }}
       aria-labelledby="aboutme-title"
@@ -21,18 +24,11 @@ const AboutMe = () => {
       <Header id="aboutme-title" heading="About Me" />
       <LineDivider width={{ base: "50%", md: "35%", lg: "18%" }} />
       <SubHeader subHeading="Introduction" />
-      <Box
-        p={{ base: "1.5rem 3rem", md: "2rem 3rem", lg: "2rem 10rem" }}
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-      >
-        <Flex as="article" gap={{ base: "0.5rem", md: "1rem" }}>
-          <AboutMeWrapper />
-        </Flex>
-        <AboutMeText />
-        <ContactMeBtn />
-      </Box>
+      <Flex mt="2rem" as="article" gap={{ base: "0.5rem", md: "1rem" }}>
+        <AboutMeWrapper />
+      </Flex>
+      <AboutMeText />
+      <ContactMeBtn />
     </Box>
   );
 };
