f67c373ca72eca720ea5f2bbf71b0e3dab76a245
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-09-03 14:06:06 +0545

    style: padding modified

--- components/templates/Navbar/Navbar.jsx
+++ components/templates/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
       display="flex"
       alignItems="center"
       justifyContent="space-between"
-      p={{ base: "0.8rem 2rem", md: "1rem 4rem", lg: "1rem 6rem" }}
+      p={{ base: "0.5rem 2rem", md: "0.5rem 4rem", lg: "0.5rem 6rem" }}
       position="relative"
       zIndex="999"
     >
