a4a14a89597932f657fe5f494fb7504aa19e71d2
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-09-07 21:54:42 +0545

    feat: ServerError named export added

--- components/layouts/index.js
+++ components/layouts/index.js
@@ -2,3 +2,4 @@ export { default as Error } from "./Error/Error";
 export { default as ThankYou } from "./ThankYou/ThankYou";
 export { default as Layout } from "./Layout/Layout";
 export { default as LoadingAnimation } from "./LoadingAnimation/LoadingAnimation";
+export { default as ServerError } from "./ServerError/ServerError";
