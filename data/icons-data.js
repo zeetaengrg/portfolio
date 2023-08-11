c6453cc95c9bfb040b12e395bdbca9790218aba8
Jiten Gurung <zeetaen.gurung@gmail.com>
2023-08-11 10:05:46 +0100

    feat: new skills added

--- data/icons-data.js
+++ data/icons-data.js
@@ -1,104 +1,119 @@
 export const language = [
-  {
-    id: 1,
-    name: "HTML5",
-    image: "/images/html.svg",
-  },
-  {
-    id: 2,
-    name: "CSS3",
-    image: "/images/css.svg",
-  },
-  {
-    id: 3,
-    name: "JavaScript",
-    image: "/images/javascript.svg",
-  },
+	{
+		id: 1,
+		name: "HTML5",
+		image: "/images/html.svg",
+	},
+	{
+		id: 2,
+		name: "CSS3",
+		image: "/images/css.svg",
+	},
+	{
+		id: 3,
+		name: "JavaScript",
+		image: "/images/javascript.svg",
+	},
+	{
+		id: 4,
+		name: "TypeScript",
+		image: "/images/typescript.svg",
+	},
 ];
 
 export const backend = [
-  {
-    id: 1,
-    name: "NodeJS",
-    image: "/images/nodejs.svg",
-  },
-  {
-    id: 2,
-    name: "ExpressJS",
-    image: "/images/expressjs.svg",
-  },
+	{
+		id: 1,
+		name: "NodeJS",
+		image: "/images/nodejs.svg",
+	},
+	{
+		id: 2,
+		name: "ExpressJS",
+		image: "/images/expressjs.svg",
+	},
 ];
 
 export const database = [
-  {
-    id: 1,
-    name: "MongoDB",
-    image: "/images/mongodb.svg",
-  },
+	{
+		id: 1,
+		name: "MongoDB",
+		image: "/images/mongodb.svg",
+	},
 ];
 
 export const library = [
-  {
-    id: 1,
-    name: "ReactJS",
-    image: "/images/reactjs.svg",
-  },
-  {
-    id: 2,
-    name: "NextJS",
-    image: "/images/nextjs.svg",
-  },
-  {
-    id: 3,
-    name: "Sass",
-    image: "/images/sass.svg",
-  },
-  {
-    id: 4,
-    name: "Styled Components",
-    image: "/images/styled-components.svg",
-  },
-  {
-    id: 5,
-    name: "Chakra-UI",
-    image: "/images/chakraui.svg",
-  },
-  // {
-  //   id: 6,
-  //   name: "Tailwind CSS",
-  //   image: "/images/tailwind.svg",
-  // },
+	{
+		id: 1,
+		name: "ReactJS",
+		image: "/images/reactjs.svg",
+	},
+	{
+		id: 2,
+		name: "NextJS",
+		image: "/images/nextjs.svg",
+	},
+	{
+		id: 3,
+		name: "Sass",
+		image: "/images/sass.svg",
+	},
+	{
+		id: 4,
+		name: "Styled Components",
+		image: "/images/styled-components.svg",
+	},
+	{
+		id: 5,
+		name: "Chakra-UI",
+		image: "/images/chakraui.svg",
+	},
+	{
+		id: 6,
+		name: "Tailwind CSS",
+		image: "/images/tailwind.svg",
+	},
 ];
 
 export const tools = [
-  {
-    id: 1,
-    name: "Yarn",
-    image: "/images/yarn.svg",
-  },
-  {
-    id: 2,
-    name: "Redux Toolkit",
-    image: "/images/redux.svg",
-  },
-  {
-    id: 3,
-    name: "Framer Motion",
-    image: "/images/framer-motion.png",
-  },
-  {
-    id: 4,
-    name: "Git",
-    image: "/images/git.svg",
-  },
-  {
-    id: 5,
-    name: "GitHub",
-    image: "/images/github.svg",
-  },
-  {
-    id: 6,
-    name: "VS Code",
-    image: "/images/vscode.svg",
-  },
+	{
+		id: 1,
+		name: "Yarn",
+		image: "/images/yarn.svg",
+	},
+	{
+		id: 2,
+		name: "npm",
+		image: "/images/npm.svg",
+	},
+	{
+		id: 3,
+		name: "Redux Toolkit",
+		image: "/images/redux.svg",
+	},
+	{
+		id: 4,
+		name: "Framer Motion",
+		image: "/images/framer-motion.png",
+	},
+	{
+		id: 5,
+		name: "Git",
+		image: "/images/git.svg",
+	},
+	{
+		id: 6,
+		name: "GitHub",
+		image: "/images/github.svg",
+	},
+	{
+		id: 7,
+		name: "MongoDB",
+		image: "/images/mongodb.svg",
+	},
+	{
+		id: 8,
+		name: "VS Code",
+		image: "/images/vscode.svg",
+	},
 ];
