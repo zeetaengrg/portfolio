09d2de4eddf2a59510a352423c2023f96732002c
Jiten Gurung <zeetaen.gurung@gmail.com>
2023-09-11 10:51:38 +0100

    refactor: color added to the text of button

--- components/elements/Btn/BtnStyle.js
+++ components/elements/Btn/BtnStyle.js
@@ -1,15 +1,16 @@
 export const btnStyle = {
-  padding: "0rem 1rem",
-  backgroundColor: "#141E39",
-  fontSize: "1.2rem",
-  fontWeight: "700",
-  transition: "all 0.3s ease",
-  _hover: {
-    bg: "#a5abbd",
-    color: "#0b111e",
-  },
-  _focus: { outline: "none" },
-  _active: { bg: "#a5abbd", color: "#0b111e" },
-  boxShadow:
-    "4px 4px 8px rgba(0, 0, 0, 0.27), 0 0 40px rgba(0, 0, 0, 0.06) inset",
+	padding: "0rem 1rem",
+	backgroundColor: "#141E39",
+	fontSize: "1.2rem",
+	fontWeight: "700",
+	transition: "all 0.3s ease",
+	_hover: {
+		bg: "#a5abbd",
+		color: "#0b111e",
+	},
+	color: "#a5abbd",
+	_focus: { outline: "none" },
+	_active: { bg: "#a5abbd", color: "#0b111e" },
+	boxShadow:
+		"4px 4px 8px rgba(0, 0, 0, 0.27), 0 0 40px rgba(0, 0, 0, 0.06) inset",
 };
