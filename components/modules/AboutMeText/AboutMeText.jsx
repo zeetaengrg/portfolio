4380dda8834dd471f35d6a42fe333b657c97d22b
Jiten Gurung <zeetaen.gurung@gmail.com>
2023-09-11 09:19:03 +0100

    feat: about me text modified

--- components/modules/AboutMeText/AboutMeText.jsx
+++ components/modules/AboutMeText/AboutMeText.jsx
@@ -8,65 +8,65 @@ import { externalLinkStyle } from "@components/elements/ExternalLinkStyle";
 const MotionBox = motion(Box);
 
 const AboutMeText = () => {
-  return (
-    <MotionBox
-      as="article"
-      p={{ base: "1.5rem 2rem", md: "1.5rem 5rem", lg: "1.5rem 15rem" }}
-      textAlign="center"
-      fontSize={{ base: "1.1rem" }}
-      variants={commonVariants}
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
-      transition={{
-        delay: 0.5,
-        duration: 0.5,
-      }}
-    >
-      <Text>
-        My interest in technologies started when I got my hands on a Toshiba
-        laptop in grade five. The curiosity of how things actually work has led
-        me to work on networks & systems engineering space. As a huge proponent
-        of evolving techonologies and exploring it, I have a passion for
-        learning and building things.
-      </Text>
-      <Text m="0.5rem 0rem">
-        Without being ostentacious, to further prove my hardworking habitude,
-        during my undergrad, I along with my team of three were awarded with the
-        Best Software Project Award by&nbsp;
-        <Link
-          href="https://nta.gov.np"
-          sx={externalLinkStyle}
-          rel="noreferrer noopener nofollow"
-          target="_blank"
-          aria-label="Link to Nepal Telecommunication Authority(NTA) website"
-        >
-          Nepal Telecommunication Authority(NTA)
-        </Link>
-        &nbsp;for building Class Routine Management System.
-      </Text>
-      {/* <Text
-        fontSize="1.1rem"
-        fontWeight="600"
-        display={{ base: "none", md: "block" }}
-      >
-        Web Development Curriculums:
-      </Text>
-      <UnorderedList
-        m="0.25rem 0rem"
-        display={{ base: "none", md: "flex" }}
-        flexDirection="column"
-        gap={{ base: "0.75rem", md: "0.25rem", lg: "0rem" }}
-      >
-        {curriculums.map((curriculum) => (
-          <CurriculumList key={curriculum.id} curriculum={curriculum} />
-        ))}
-      </UnorderedList>
-      <Box as="figure" display={{ base: "block", md: "none" }}>
-        <CurriculumTable />
-      </Box> */}
-    </MotionBox>
-  );
+	return (
+		<MotionBox
+			as="article"
+			p={{ base: "1.5rem 2rem", md: "1.5rem 5rem", lg: "1.5rem 15rem" }}
+			textAlign="center"
+			fontSize={{ base: "1.1rem" }}
+			variants={commonVariants}
+			initial="hidden"
+			whileInView="visible"
+			viewport={{ once: true }}
+			transition={{
+				delay: 0.5,
+				duration: 0.5,
+			}}
+		>
+			<Text>
+				My interest in technologies started when I got my hands on a Toshiba
+				laptop in grade five. The curiosity of how things actually work has led
+				me to embark on the field of computer science and technology. As a huge
+				proponent of evolving techonologies and exploring it, I have a passion
+				for learning and building things.
+			</Text>
+			<Text m="0.5rem 0rem">
+				Without being ostentacious, to further prove my hardworking habitude,
+				during my undergrad, I along with my team of three were awarded with the
+				Best Software Project Award by&nbsp;
+				<Link
+					href="https://nta.gov.np"
+					sx={externalLinkStyle}
+					rel="noreferrer noopener nofollow"
+					target="_blank"
+					aria-label="Link to Nepal Telecommunication Authority(NTA) website"
+				>
+					Nepal Telecommunication Authority(NTA)
+				</Link>
+				&nbsp;for building Class Routine Management System.
+			</Text>
+			<Text
+				fontSize="1.1rem"
+				fontWeight="600"
+				display={{ base: "none", md: "block" }}
+			>
+				Web Development Curriculums:
+			</Text>
+			<UnorderedList
+				m="0.25rem 0rem"
+				display={{ base: "none", md: "flex" }}
+				flexDirection="column"
+				gap={{ base: "0.75rem", md: "0.25rem", lg: "0rem" }}
+			>
+				{curriculums.map((curriculum) => (
+					<CurriculumList key={curriculum.id} curriculum={curriculum} />
+				))}
+			</UnorderedList>
+			<Box as="figure" display={{ base: "block", md: "none" }}>
+				<CurriculumTable />
+			</Box>
+		</MotionBox>
+	);
 };
 
 export default AboutMeText;
